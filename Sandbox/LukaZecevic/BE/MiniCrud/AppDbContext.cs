7bf82fd99d79470fd9f550e8a37ad999b0e257a3
luka.zecevic <44-2020@pmf.kg.ac.rs>
2024-02-29 16:25:59 +0100

    dodat demo

---
+++ Sandbox/LukaZecevic/BE/MiniCrud/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MiniCrud.Models;
+
+namespace MiniCrud;
+
+// Sqlite server from https://learn.microsoft.com/en-us/ef/core/get-started/overview/first-app?tabs=netcore-cli
+public class AppDbContext : DbContext
+{
+    public DbSet<Item> Items { get; set; }
+
+    public string DbPath { get; }
+    public AppDbContext()
+    {
+        var folder = Environment.SpecialFolder.LocalApplicationData;
+        var path = Environment.GetFolderPath(folder);
+        DbPath = System.IO.Path.Join(path, "blogging.db");
+    }
+    // The following configures EF to create a Sqlite database file in the
+    // special "local" folder for your platform.
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+        => options.UseSqlite($"Data Source={DbPath}");
+}
