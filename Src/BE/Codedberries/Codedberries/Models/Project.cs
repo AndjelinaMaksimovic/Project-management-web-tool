90ba4eedc7058d678b249c1492219b430706d702
uros.aksentijevic <24-2021@pmf.kg.ac.rs>
2024-03-10 17:49:41 +0100

    #15 Dodat model Project

---
+++ Src/BE/Codedberries/Codedberries/Models/Project.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Codedberries.Models
+{
+    public class Project
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        public required string Name { get; set; }
+    }
+}
