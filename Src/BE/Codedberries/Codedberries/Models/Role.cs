05d47d2851edee311382bd738c8640a06e2d8937
uros.aksentijevic <24-2021@pmf.kg.ac.rs>
2024-03-10 17:56:04 +0100

    #15 Dodat model Role

---
+++ Src/BE/Codedberries/Codedberries/Models/Role.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Codedberries.Models
+{
+    public class Role
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        public required string Name { get; set; }
+    }
+}
