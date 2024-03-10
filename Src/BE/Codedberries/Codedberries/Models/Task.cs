ffd65a4f5b4bd8b2b114d45be9180fc09bbe5ece
uros.aksentijevic <24-2021@pmf.kg.ac.rs>
2024-03-10 17:58:45 +0100

    #15 Dodat model Task

---
+++ Src/BE/Codedberries/Codedberries/Models/Task.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Codedberries.Models
+{
+    public class Task
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        public required string Description { get; set; }
+
+        public DateTime DueDate { get; set; }
+    }
+}
