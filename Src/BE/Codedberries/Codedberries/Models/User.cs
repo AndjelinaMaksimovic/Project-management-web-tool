50edc22d5e12205ea9f726eebc7b9e0dd6ce5391
uros.aksentijevic <24-2021@pmf.kg.ac.rs>
2024-03-10 18:04:31 +0100

    #15 Dodat model User

---
+++ Src/BE/Codedberries/Codedberries/Models/User.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Codedberries.Models
+{
+    public class User
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [EmailAddress]
+        [Required]
+        public required string Email { get; set; }
+
+        [Required]
+        public required string Password { get; set; }
+
+        [Required]
+        public required string Firstname { get; set; }
+
+        [Required]
+        public required string Lastname { get; set; }
+
+        [ForeignKey("RoleId")]
+        public int RoleId { get; set; }
+    }
+}
