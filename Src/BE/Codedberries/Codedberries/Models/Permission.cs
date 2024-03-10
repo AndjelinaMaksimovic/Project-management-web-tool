815b728bbbd78abe27b6851fa2ab5220ee016733
uros.aksentijevic <24-2021@pmf.kg.ac.rs>
2024-03-10 18:00:04 +0100

    #15 Dodat model Permission

---
+++ Src/BE/Codedberries/Codedberries/Models/Permission.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Codedberries.Models
+{
+    public class Permission
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        public string? Description { get; set; }
+    }
+}
