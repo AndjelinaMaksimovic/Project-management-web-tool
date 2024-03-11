ba1db78eff9fce4618502bb73ca3334496bcdd95
Milica Rogic <milica.rog@gmail.com>
2024-03-11 19:15:01 +0100

    #23 Dodat Session model

---
+++ Src/BE/Codedberries/Codedberries/Models/Session.cs
@@ -0,0 +1,10 @@
+﻿namespace Codedberries.Models
+{
+    public class Session
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public string? Token { get; set; }
+        public DateTime ExpirationTime { get; set; }
+    }
+}
