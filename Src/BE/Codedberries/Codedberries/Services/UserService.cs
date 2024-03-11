c06ddf6722ae9226b9e58e63d397d61481e2f267
Milica Rogic <milica.rog@gmail.com>
2024-03-11 19:20:53 +0100

    #23 Kreiran UserService servis

---
+++ Src/BE/Codedberries/Codedberries/Services/UserService.cs
@@ -0,0 +1,17 @@
+﻿namespace Codedberries.Services
+{
+    public class UserService
+    {
+        private const int SaltSize = 16;
+        private const int KeySize = 32;
+        private const int Iterations = 10000;
+
+        private readonly AppDatabaseContext _databaseContext;
+        private const int SessionDurationHours = 24;
+
+        public UserService(AppDatabaseContext databaseContext)
+        {
+            _databaseContext = databaseContext;
+        }
+    }
+}
