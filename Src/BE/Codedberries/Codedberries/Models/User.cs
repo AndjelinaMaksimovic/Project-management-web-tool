74d0cdec1890438e9ab981c9476c882cb90a300d
Milica Rogic <milica.rog@gmail.com>
2024-03-11 19:11:03 +0100

    #23 Mala izmena u Models\User.cs

--- Src/BE/Codedberries/Codedberries/Models/User.cs
+++ Src/BE/Codedberries/Codedberries/Models/User.cs
@@ -2,9 +2,10 @@
 
 public class User
 {
-    private int UserId { get; set; }
+    public int UserId { get; set; }
     private string? Username { get; set; }
     public string? Password { get; set; }
+    public byte[]? PasswordSalt { get; set; }
     public string? Email { get; set; }
     private int RoleID { get; set; }
 
