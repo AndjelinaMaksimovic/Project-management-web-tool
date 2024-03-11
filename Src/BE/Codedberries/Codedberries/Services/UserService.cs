0458f3067ff22217e96e3e2defe34a642ef63de1
Milica Rogic <milica.rog@gmail.com>
2024-03-11 19:24:52 +0100

    #23 Dodata metoda "VerifyPassword" u UserService

--- Src/BE/Codedberries/Codedberries/Services/UserService.cs
+++ Src/BE/Codedberries/Codedberries/Services/UserService.cs
@@ -1,4 +1,6 @@
-﻿namespace Codedberries.Services
+﻿using System.Security.Cryptography;
+
+namespace Codedberries.Services
 {
     public class UserService
     {
@@ -12,6 +14,28 @@
         public UserService(AppDatabaseContext databaseContext)
         {
             _databaseContext = databaseContext;
+        }
+
+        private bool VerifyPassword(string password, string hashedPassword, byte[] salt)
+        {
+            byte[] hashBytes = Convert.FromBase64String(hashedPassword);
+            byte[] saltFromHash = new byte[SaltSize];
+            Array.Copy(hashBytes, 0, saltFromHash, 0, SaltSize);
+
+            using (var pbkdf2 = new Rfc2898DeriveBytes(password, saltFromHash, Iterations))
+            {
+                byte[] key = pbkdf2.GetBytes(KeySize);
+
+                for (int i = 0; i < KeySize; i++)
+                {
+                    if (key[i] != hashBytes[i + SaltSize])
+                    {
+                        return false; // wrong password
+                    }
+                }
+            }
+
+            return true;
         }
     }
 }
