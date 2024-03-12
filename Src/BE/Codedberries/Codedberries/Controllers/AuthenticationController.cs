87d427ec5dad1db685fcc41ebba8c2f1fe41e2fa
Milica Rogic <milica.rog@gmail.com>
2024-03-12 08:11:29 +0100

    #23 Dodat AuthenticationController

---
+++ Src/BE/Codedberries/Codedberries/Controllers/AuthenticationController.cs
@@ -0,0 +1,42 @@
+﻿using Codedberries.Models;
+using Codedberries.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Codedberries.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        private readonly UserService _userService;
+
+        public AuthenticationController(UserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpPost("login/{email}/{password}")]
+        public IActionResult Login(string email, string password)
+        {
+            Session currentSession = _userService.LoginUser(email, password);
+
+            if (currentSession != null)
+            {
+                return Ok(new { sessionId = currentSession.Token });
+            }
+
+            return Unauthorized(new { message = "Login fail!" });
+        }
+
+        [HttpPost("logout/{token}")]
+        public IActionResult Logout(string token)
+        {
+            if (_userService.LogoutUser(token))
+            {
+                return Ok(new { message = "Logout successful!" });
+            }
+            return BadRequest(new { message = "Logout fail!" });
+        }
+    }
+}
