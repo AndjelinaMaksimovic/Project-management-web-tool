fd2a34d220b9d8597da5ee0a5698263f886f68ad
Strahinja Milenovic <59-2021@pmf.kg.ac.rs>
2024-03-13 20:11:56 +0100

    #28 implement registration page

--- Src/FE/angular-app/src/app/pages/register/register.component.ts
+++ Src/FE/angular-app/src/app/pages/register/register.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
-
+import { MaterialModule } from '../../material/material.module';
+import { AuthService } from '../../services/auth.service';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [MaterialModule, FormsModule, CommonModule],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.css'
+  styleUrl: './register.component.css',
 })
 export class RegisterComponent {
-
+  email: string = '';
+  password: string = '';
+  passwordConfirm: string = '';
+  errorMessage: string | null = null;
+  constructor(private authService: AuthService, private router: Router) {}
+  register() {
+    console.log('register');
+  }
 }
