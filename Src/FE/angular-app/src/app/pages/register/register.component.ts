3a58818ffbafded1bd5aa90e26cb58e25d5345c3
Strahinja Milenovic <59-2021@pmf.kg.ac.rs>
2024-03-13 21:13:02 +0100

    #28 read register query parameters

--- Src/FE/angular-app/src/app/pages/register/register.component.ts
+++ Src/FE/angular-app/src/app/pages/register/register.component.ts
@@ -3,7 +3,7 @@ import { MaterialModule } from '../../material/material.module';
 import { AuthService } from '../../services/auth.service';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 @Component({
   selector: 'app-register',
   standalone: true,
@@ -12,19 +12,48 @@ import { Router } from '@angular/router';
   styleUrl: './register.component.css',
 })
 export class RegisterComponent {
-  email: string = '';
   password: string = '';
   passwordConfirm: string = '';
   errorMessage: string | null = null;
-  constructor(private authService: AuthService, private router: Router) {}
+
+  // query params
+  token: string | undefined;
+  email: string | undefined;
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
+  // listen to query parameters
+  ngOnInit() {
+    this.route.queryParams.subscribe((params) => {
+      this.token = params['token'];
+      this.email = params['email'];
+      console.log(params); // Logs the query parameters
+    });
+  }
   async register() {
     console.log('register');
+    // check token
+    if (this.token === undefined) {
+      this.errorMessage = 'no token';
+      return;
+    }
+    // check email
+    if (this.email === undefined) {
+      this.errorMessage = 'no email passed';
+      return;
+    }
     // check if password confirm matches
     if (this.password !== this.passwordConfirm) {
       this.errorMessage = 'passwords do not match!';
       return;
     }
-    const res = await this.authService.register('test', 'test', '1234');
+    const res = await this.authService.register(
+      this.token,
+      this.email,
+      this.password
+    );
     // if registration fails return
     if (!res) {
       console.log('registration failed');
