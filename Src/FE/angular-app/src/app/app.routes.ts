919ab20fa6f135ebea96a21025630dab85de6005
Strahinja Milenovic <59-2021@pmf.kg.ac.rs>
2024-03-14 15:04:36 +0100

    #29 add activate route

--- Src/FE/angular-app/src/app/app.routes.ts
+++ Src/FE/angular-app/src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { LoginComponent } from './pages/login/login.component';
 import { HomeComponent } from './pages/home/home.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { ErrorComponent } from './pages/error/error.component';
+import { ActivateComponent } from './pages/activate/activate.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: '**', component: ErrorComponent }, 
+  { path: 'activate', component: ActivateComponent },
+  { path: '**', component: ErrorComponent },
 ];
