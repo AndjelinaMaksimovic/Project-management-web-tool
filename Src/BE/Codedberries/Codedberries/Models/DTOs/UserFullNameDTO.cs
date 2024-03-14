c5af64deabb8013033081e79d080164259eb1bb8
Milica Rogic <milica.rog@gmail.com>
2024-03-14 22:15:35 +0100

    #40 Dodat UserFullNameDTO

---
+++ Src/BE/Codedberries/Codedberries/Models/DTOs/UserFullNameDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace Codedberries.Models.DTOs
+{
+    public class UserFullNameDTO
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
