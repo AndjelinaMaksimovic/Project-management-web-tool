2b43b8b9d56a378d220a53992807e34f630a562d
Milica Rogic <milica.rog@gmail.com>
2024-03-14 22:34:07 +0100

    #40 Dodat RequestService i GetUserFullNameById metoda

---
+++ Src/BE/Codedberries/Codedberries/Services/RequestsService.cs
@@ -0,0 +1,32 @@
+﻿using Codedberries.Models;
+using Codedberries.Models.DTOs;
+using Microsoft.EntityFrameworkCore;
+
+namespace Codedberries.Services
+{
+    public class RequestsService
+    {
+        private readonly AppDatabaseContext _databaseContext;
+
+        public RequestsService(AppDatabaseContext databaseContext)
+        {
+            _databaseContext = databaseContext;
+        }
+
+        public UserFullNameDTO GetUserFullNameById(int userId)
+        {
+            User user = _databaseContext.Users.FirstOrDefault(u => u.Id == userId);
+            
+            if (user == null)
+            {
+                return null;
+            }
+
+            return new UserFullNameDTO
+            {
+                FirstName = user.Firstname,
+                LastName = user.Lastname
+            };
+        }
+    }
+}
