ecf84bcc0b442c6f75b67b3bb4a05d43adbd76f2
Milica Rogic <milica.rog@gmail.com>
2024-03-14 22:56:25 +0100

    #40 Kreiran RequestsController i zahtev GetUserFullName

---
+++ Src/BE/Codedberries/Codedberries/Controllers/RequestsController.cs
@@ -0,0 +1,32 @@
+﻿using Codedberries.Models.DTOs;
+using Codedberries.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Codedberries.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RequestsController : ControllerBase
+    {
+        private readonly RequestsService _requestsService;
+
+        public RequestsController(RequestsService requestsService)
+        {
+            _requestsService = requestsService;
+        }
+
+        [HttpPost("userFullName")]
+        public IActionResult GetUserFullName(UserIdDTO body)
+        {
+            UserFullNameDTO userFullName = _requestsService.GetUserFullNameById(body.UserId);
+            
+            if (userFullName == null)
+            {
+                return NotFound(new { message = "User not found!" }); // TO-DO ErrorDTO
+            }
+
+            return Ok(userFullName);
+        }
+    }
+}
