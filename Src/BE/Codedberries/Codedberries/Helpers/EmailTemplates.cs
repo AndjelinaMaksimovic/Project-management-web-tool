0040038c058f1cbbfe22b9dc47720bfe6c8d9338
uros.aksentijevic <24-2021@pmf.kg.ac.rs>
2024-03-15 17:47:56 +0100

    #34 Promenjena email poruka za aktivaciju naloga

--- Src/BE/Codedberries/Codedberries/Helpers/EmailTemplates.cs
+++ Src/BE/Codedberries/Codedberries/Helpers/EmailTemplates.cs
@@ -5,12 +5,12 @@
         public static string ActivationEmail(string firstname, string lastname, string link)
         {
             return
-                $"Dear {firstname} {lastname},\n\n" +
-                "Congratulations! Your registration was successful. Please click on the following link to activate your account:\n\n" +
-                $"{link}\n\n" +
-                "Once activated, you will be prompted to set your password.\n\n" +
-                "If you have any questions or need assistance, feel free to contact our support team at [codedberries.pm@gmail.com].\n\n" +
-                "Best regards,\n\n[Company name]\nCustomer Support Team";
+                $"Dear <b>{firstname} {lastname}</b>,<br><br>" +
+                "Congratulations! Your registration was successful.<br><br>" +
+                $"Please click on <a href='{link}'>link</a> to activate your account:<br><br>" +
+                "Once activated, you will be prompted to set your password.<br>" +
+                "If you have any questions or need assistance, feel free to contact our support team at [codedberries.pm@gmail.com].<br><br>" +
+                "Best regards,<br><br>CodedBerries team<br>Customer Support Team";
         }
     }
 }
