7c6b2e36aec12efc72f4f5575cbe9b59d10b6b52
Milica Rogic <milica.rog@gmail.com>
2024-03-15 17:24:46 +0100

    #40 Kreirani UserController i RoleController zajedno sa trazenim zahtevima

---
+++ Src/BE/Codedberries/Codedberries/Controllers/RoleController.cs
@@ -0,0 +1,32 @@
+﻿using Codedberries.Models.DTOs;
+using Codedberries.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Codedberries.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RoleController : ControllerBase
+    {
+        private readonly RoleService _roleService;
+
+        public RoleController(RoleService roleService)
+        {
+            _roleService = roleService;
+        }
+
+        [HttpGet("allRolesNames")]
+        public IActionResult GetAllRolesNames()
+        {
+            AllRolesNamesDTO allRolesNames = _roleService.GetRoleNames();
+
+            if (allRolesNames == null)
+            {
+                return NotFound(new { message = "No roles found!" }); // TO-DO ErrorDTO
+            }
+
+            return Ok(allRolesNames);
+        }
+    }
+}
---
+++ Src/BE/Codedberries/Codedberries/Controllers/UserController.cs
@@ -0,0 +1,46 @@
+﻿using Codedberries.Models.DTOs;
+using Codedberries.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Codedberries.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly UserService _userService;
+
+        public UserController(UserService userService)
+        {   
+            _userService = userService;
+        }
+
+
+        [HttpPost("userFullName")]
+        public IActionResult GetUserFullName(UserIdDTO body)
+        {
+            UserFullNameDTO userFullName = _userService.GetUserFullNameById(body.UserId);
+
+            if (userFullName == null)
+            {
+                return NotFound(new { message = "User not found!" }); // TO-DO ErrorDTO
+            }
+
+            return Ok(userFullName);
+        }
+
+        [HttpPost("userRole")]
+        public IActionResult GetUserRole(UserIdDTO body)
+        {
+            UserRoleDTO userRole = _userService.GetUserRole(body.UserId);
+
+            if (userRole == null)
+            {
+                return NotFound(new { message = "User role not found!" }); // TO-DO ErrorDTO
+            }
+
+            return Ok(userRole);
+        }
+    }
+}
