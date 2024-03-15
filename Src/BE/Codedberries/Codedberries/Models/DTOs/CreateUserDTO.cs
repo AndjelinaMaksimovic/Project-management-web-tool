f820065821e43f023d97d51b7708873d8d6dae6c
Milica Rogic <milica.rog@gmail.com>
2024-03-15 09:08:31 +0100

    #44 Dodat CreateUserDTO

---
+++ Src/BE/Codedberries/Codedberries/Models/DTOs/CreateUserDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace Codedberries.Models.DTOs
+{
+    public class CreateUserDTO
+    {
+        public string Email { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
