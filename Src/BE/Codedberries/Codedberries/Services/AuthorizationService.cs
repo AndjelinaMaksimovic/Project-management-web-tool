4ef2b9f7e90edf52dd6597d714278b012dd3cceb
Marko Nenadovic <67-2020@pmf.kg.ac.rs>
2024-03-15 23:25:54 +0100

    #21 funkcije za autorizaciju premestene u fajl AuthorizationService

---
+++ Src/BE/Codedberries/Codedberries/Services/AuthorizationService.cs
@@ -0,0 +1,197 @@
+﻿using Codedberries.Models;
+
+namespace Codedberries.Services
+{
+    public class AuthorizationService
+    {
+
+        private readonly AppDatabaseContext _databaseContext;
+
+       
+
+
+        public bool canAddNewUser(int userId)
+        {
+            var user = _databaseContext.Users.FirstOrDefault(u => u.Id == userId);
+            if (user != null && user.RoleId.HasValue)
+            {
+                var userRole = _databaseContext.Roles.FirstOrDefault(r => r.Id == user.RoleId);
+                if (userRole != null)
+                {
+                    var permission = _databaseContext.Permissions.FirstOrDefault(p => p.Id == 1);
+                    return permission != null && userRole.Permissions.Contains<Permission>(permission);
+                }
+            }
+            return false;
+        }
+
+
+        public bool canAddUserToProject(int userId)
+        {
+            var user = _databaseContext.Users.FirstOrDefault(u => u.Id == userId);
+            if (user != null && user.RoleId.HasValue)
+            {
+                var userRole = _databaseContext.Roles.FirstOrDefault(r => r.Id == user.RoleId);
+                if (userRole != null)
+                {
+                    var permission = _databaseContext.Permissions.FirstOrDefault(p => p.Id ==2 );
+                    return permission != null && userRole.Permissions.Contains<Permission>(permission);
+                }
+            }
+            return false;
+        }
+
+
+        public bool canRemoveUserFromProject(int userId)
+        {
+            var user = _databaseContext.Users.FirstOrDefault(u => u.Id == userId);
+            if (user != null && user.RoleId.HasValue)
+            {
+                var userRole = _databaseContext.Roles.FirstOrDefault(r => r.Id == user.RoleId);
+                if (userRole != null)
+                {
+                    var permission = _databaseContext.Permissions.FirstOrDefault(p => p.Id==3);
+                    return permission != null && userRole.Permissions.Contains<Permission>(permission);
+                }
+            }
+            return false;
+        }
+
+
+        public bool canCreateProject(int userId)
+        {
+            var user = _databaseContext.Users.FirstOrDefault(u => u.Id == userId);
+            if (user != null && user.RoleId.HasValue)
+            {
+                var userRole = _databaseContext.Roles.FirstOrDefault(r => r.Id == user.RoleId);
+                if (userRole != null)
+                {
+                    var permission = _databaseContext.Permissions.FirstOrDefault(p => p.Id==4);
+                    return permission != null && userRole.Permissions.Contains<Permission>(permission);
+                }
+            }
+            return false;
+        }
+
+        public bool canDeleteProject(int userId)
+        {
+            var user = _databaseContext.Users.FirstOrDefault(u => u.Id == userId);
+            if (user != null && user.RoleId.HasValue)
+            {
+                var userRole = _databaseContext.Roles.FirstOrDefault(r => r.Id == user.RoleId);
+                if (userRole != null)
+                {
+                    var permission = _databaseContext.Permissions.FirstOrDefault(p => p.Id==5);
+                    return permission != null && userRole.Permissions.Contains<Permission>(permission);
+                }
+            }
+            return false;
+        }
+
+
+        public bool canEditProject(int userId)
+        {
+            var user = _databaseContext.Users.FirstOrDefault(u => u.Id == userId);
+            if (user != null && user.RoleId.HasValue)
+            {
+                var userRole = _databaseContext.Roles.FirstOrDefault(r => r.Id == user.RoleId);
+                if (userRole != null)
+                {
+                    var permission = _databaseContext.Permissions.FirstOrDefault(p => p.Id==6);
+                    return permission != null && userRole.Permissions.Contains<Permission>(permission);
+                }
+            }
+            return false;
+        }
+
+        public bool canViewProject(int userId)
+        {
+            var user = _databaseContext.Users.FirstOrDefault(u => u.Id == userId);
+            if (user != null && user.RoleId.HasValue)
+            {
+                var userRole = _databaseContext.Roles.FirstOrDefault(r => r.Id == user.RoleId);
+                if (userRole != null)
+                {
+                    var permission = _databaseContext.Permissions.FirstOrDefault(p => p.Id==7);
+                    return permission != null && userRole.Permissions.Contains<Permission>(permission);
+                }
+            }
+            return false;
+        }
+
+        public bool canAddRoleAndPermissionToUser(int userId)
+        {
+            var user = _databaseContext.Users.FirstOrDefault(u => u.Id == userId);
+            if (user != null && user.RoleId.HasValue)
+            {
+                var userRole = _databaseContext.Roles.FirstOrDefault(r => r.Id == user.RoleId);
+                if (userRole != null)
+                {
+                    var permission = _databaseContext.Permissions.FirstOrDefault(p => p.Id==11);
+                    return permission != null && userRole.Permissions.Contains<Permission>(permission);
+                }
+            }
+            return false;
+        }
+
+        public bool canAddTaskToUser(int userId)
+        {
+            var user = _databaseContext.Users.FirstOrDefault(u => u.Id == userId);
+            if (user != null && user.RoleId.HasValue)
+            {
+                var userRole = _databaseContext.Roles.FirstOrDefault(r => r.Id == user.RoleId);
+                if (userRole != null)
+                {
+                    var permission = _databaseContext.Permissions.FirstOrDefault(p => p.Id == 12);
+                    return permission != null && userRole.Permissions.Contains<Permission>(permission);
+                }
+            }
+            return false;
+        }
+
+        public bool canCreateTask(int userId)
+        {
+            var user = _databaseContext.Users.FirstOrDefault(u => u.Id == userId);
+            if (user != null && user.RoleId.HasValue)
+            {
+                var userRole = _databaseContext.Roles.FirstOrDefault(r => r.Id == user.RoleId);
+                if (userRole != null)
+                {
+                    var permission = _databaseContext.Permissions.FirstOrDefault(p => p.Id==8);
+                    return permission != null && userRole.Permissions.Contains<Permission>(permission);
+                }
+            }
+            return false;
+        }
+
+        public bool canRemoveTask(int userId)
+        {
+            var user = _databaseContext.Users.FirstOrDefault(u => u.Id == userId);
+            if (user != null && user.RoleId.HasValue)
+            {
+                var userRole = _databaseContext.Roles.FirstOrDefault(r => r.Id == user.RoleId);
+                if (userRole != null)
+                {
+                    var permission = _databaseContext.Permissions.FirstOrDefault(p => p.Id==9);
+                    return permission != null && userRole.Permissions.Contains<Permission>(permission);
+                }
+            }
+            return false;
+        }
+
+        public bool canEditTask(int userId)
+        {
+            var user = _databaseContext.Users.FirstOrDefault(u => u.Id == userId);
+            if (user != null && user.RoleId.HasValue)
+            {
+                var userRole = _databaseContext.Roles.FirstOrDefault(r => r.Id == user.RoleId);
+                if (userRole != null)
+                {
+                    var permission = _databaseContext.Permissions.FirstOrDefault(p => p.Id == 10);
+                    return permission != null && userRole.Permissions.Contains<Permission>(permission);
+                }
+            }
+            return false;
+        }
+    }
+}
