7f82b72dde10f11c93247dd565ec0fecac2eed47
Milica Rogic <milica.rog@gmail.com>
2024-03-15 09:11:58 +0100

    #44 Dodat ActivateAccountDTO

---
+++ Src/BE/Codedberries/Codedberries/Models/DTOs/ActivateAccountDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace Codedberries.Models.DTOs
+{
+    public class ActivateAccountDTO
+    {
+        public string Token { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
