3b553d1eaae6441d3cd57d2ecfee90c3aaf640f3
uros.aksentijevic <24-2021@pmf.kg.ac.rs>
2024-03-17 01:18:34 +0100

    #39 Dodat DTO za menjanje profilne slike

---
+++ Src/BE/Codedberries/Codedberries/Models/DTOs/ProfilePictureDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace Codedberries.Models.DTOs
+{
+    public class ProfilePictureDTO
+    {
+        public int UserId { get; set; }
+        public string? ProfilePicture { get; set; }
+    }
+}
