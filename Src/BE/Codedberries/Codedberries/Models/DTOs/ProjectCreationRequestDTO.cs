74304098dc1c812b56b189852a825771003ff083
Milica Rogic <milica.rog@gmail.com>
2024-03-17 11:57:18 +0100

    #50 Dodat ProjectCreationRequestDTO

---
+++ Src/BE/Codedberries/Codedberries/Models/DTOs/ProjectCreationRequestDTO.cs
@@ -0,0 +1,14 @@
+﻿using Codedberries.Helpers;
+
+namespace Codedberries.Models.DTOs
+{
+    public class ProjectCreationRequestDTO
+    {
+        public string UserId { get; set; }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int? ParentProjectId { get; set; }
+        public List<int> UserIds { get; set; }
+    }
+}
