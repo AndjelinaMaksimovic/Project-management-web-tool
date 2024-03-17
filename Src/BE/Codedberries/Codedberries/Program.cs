a0c6c848c17ea5630a0499e0313bf093a1d41c06
Milica Rogic <milica.rog@gmail.com>
2024-03-17 14:44:34 +0100

    #50 Registrovani servisi koji su nedostajali

--- Src/BE/Codedberries/Codedberries/Program.cs
+++ Src/BE/Codedberries/Codedberries/Program.cs
@@ -25,6 +25,8 @@ namespace Codedberries
             builder.Services.AddSingleton<Config>();
             builder.Services.AddScoped<AuthorizationService>();
             builder.Services.AddScoped<ProjectService>();
+            builder.Services.AddScoped<RoleService>();
+            builder.Services.AddScoped<MailService>();
 
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
