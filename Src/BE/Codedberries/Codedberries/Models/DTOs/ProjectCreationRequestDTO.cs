6f4d15d26a4024eedcd30bbec8fd477771648cb8
Milica Rogic <milica.rog@gmail.com>
2024-03-17 18:37:03 +0100

    #50 Uklonjen UserId iz ProjectCreationRequestDTO

--- Src/BE/Codedberries/Codedberries/Models/DTOs/ProjectCreationRequestDTO.cs
+++ Src/BE/Codedberries/Codedberries/Models/DTOs/ProjectCreationRequestDTO.cs
@@ -4,7 +4,6 @@ namespace Codedberries.Models.DTOs
 {
     public class ProjectCreationRequestDTO
     {
-        public int UserId { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
         public int? ParentProjectId { get; set; }
