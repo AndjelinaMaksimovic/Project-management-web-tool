9de0b37599fd2929def7f8ffd6c12ace7cc70bf5
Milica Rogic <milica.rog@gmail.com>
2024-03-17 12:06:22 +0100

    #50 Kreiran servis ProjectService

--- Src/BE/Codedberries/Codedberries/Models/DTOs/ProjectCreationRequestDTO.cs
+++ Src/BE/Codedberries/Codedberries/Models/DTOs/ProjectCreationRequestDTO.cs
@@ -4,7 +4,7 @@ namespace Codedberries.Models.DTOs
 {
     public class ProjectCreationRequestDTO
     {
-        public string UserId { get; set; }
+        public int UserId { get; set; }
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
---
+++ Src/BE/Codedberries/Codedberries/Services/ProjectService.cs
@@ -0,0 +1,45 @@
+﻿using Codedberries.Models.DTOs;
+using Codedberries.Models;
+
+namespace Codedberries.Services
+{
+    public class ProjectService
+    {
+        private readonly AppDatabaseContext _databaseContext;
+        private readonly AuthorizationService _authorizationService;
+
+        public ProjectService(AppDatabaseContext databaseContext, AuthorizationService authorizationService)
+        {
+            _databaseContext = databaseContext;
+            _authorizationService = authorizationService;
+        }
+
+        public async Task<Project> CreateProject(ProjectCreationRequestDTO request)
+        {
+            if (!_authorizationService.canCreateTask(request.UserId))
+            {
+                throw new UnauthorizedAccessException("User does not have permission to create a project!");
+            }
+
+            Project project = new Project(request.Name, request.ParentProjectId, request.Description);
+
+            if (request.UserIds != null && request.UserIds.Any())
+            {
+                foreach (int userId in request.UserIds)
+                {
+                    User user = _databaseContext.Users.FirstOrDefault(u => u.Id == userId);
+
+                    if (user != null)
+                    {
+                        project.Users.Add(user);
+                    }
+                }
+            }
+
+            _databaseContext.Projects.Add(project);
+            await _databaseContext.SaveChangesAsync();
+
+            return project;
+        }
+    }
+}
