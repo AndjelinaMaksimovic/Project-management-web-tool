6d51c6b60935fba39ee836ba879c23e9c37de9bf
Marko Nenadovic <67-2020@pmf.kg.ac.rs>
2024-03-18 21:05:31 +0100

    #54 dodat taskInfoDTO

--- Src/BE/Codedberries/Codedberries/Models/DTOs/TaskCreationRequestDTO.cs
+++ Src/BE/Codedberries/Codedberries/Models/DTOs/TaskCreationRequestDTO.cs
@@ -1,6 +1,16 @@
-﻿namespace Codedberries.Models.DTOs
+﻿using Codedberries.Common;
+
+namespace Codedberries.Models.DTOs
 {
     public class TaskCreationRequestDTO
     {
+        public string Description { get; set; }
+        public DateTime DueDate { get; set; }
+        public int UserId { get; set; }
+        public StatusEnum Status { get; set; }
+        public PriorityEnum Priority { get; set; }
+        public int DifficultyLevel { get; set; }
+        public int CategoryId { get; set; }
+        public List<int> DependencyIds { get; set; }
     }
 }
