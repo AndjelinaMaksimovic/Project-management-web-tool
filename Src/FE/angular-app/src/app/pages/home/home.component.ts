fb50366da6b441146d27686b530ee28dfb96d482
luka.zecevic <44-2020@pmf.kg.ac.rs>
2024-03-18 21:47:47 +0100

    #37 project owner page prototip

--- Src/FE/angular-app/src/app/pages/home/home.component.ts
+++ Src/FE/angular-app/src/app/pages/home/home.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
+import { NavbarComponent } from '../../components/navbar/navbar.component';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon'
+import { MatBadgeModule } from '@angular/material/badge'
+import { DatePipe } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { NgStyle } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [ NavbarComponent, MatSidenavModule, MatIconModule, MatBadgeModule, DatePipe, MatButtonModule, NgStyle ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
@@ -13,4 +20,24 @@ export class HomeComponent {
   constructor(private authService: AuthService) {
     this.username = authService.currentUserValue?.email || '';
   }
+
+  
+  user = {
+    name: "Petar",
+    surname: "Petrovic"
+  }
+  date = new Date()
+
+  projects = [
+    {
+      title: 'Project 1',
+      desc: 'Lorem ipsum dolor sit',
+      progress: 48
+    },
+    {
+      title: 'Project 1',
+      desc: 'Lorem ipsum dolor sit',
+      progress: 48
+    }
+  ]
 }
