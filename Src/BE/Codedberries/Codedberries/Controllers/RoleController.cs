d3f1ea8d984438e5a682e02be3a33e79288bf421
Milica Rogic <milica.rog@gmail.com>
2024-03-18 16:13:35 +0100

    #57 Kreiran zahtev CreateCustomRole u RoleController-u

--- Src/BE/Codedberries/Codedberries/Controllers/RoleController.cs
+++ Src/BE/Codedberries/Codedberries/Controllers/RoleController.cs
@@ -29,5 +29,27 @@ namespace Codedberries.Controllers
 
             return Ok(allRolesNames);
         }
+
+        [HttpPost("createCustomRole")]
+        public async Task<IActionResult> CreateCustomRole([FromBody] CustomRoleDTO body)
+        {
+            try
+            {
+                var result = await _roleService.AddNewCustomRole(HttpContext, body);
+
+                if (result)
+                {
+                    return Ok("Custom role created successfully!");
+                }
+                else
+                {
+                    return BadRequest(new ErrorMsg("Failed to create custom role!"));
+                }
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, new ErrorMsg("Internal server error: " + ex.Message));
+            }
+        }
     }
 }
