14ed5ef66f2eaeec50053b5547784774a862b005
Marko Nenadovic <67-2020@pmf.kg.ac.rs>
2024-03-18 21:05:31 +0100

    #54 dodat taskInfoDTO

---
+++ Src/BE/Codedberries/Codedberries/Models/DTOs/TaskInfoDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace Codedberries.Models.DTOs
+{
+    public class TaskInfoDTO
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public DateTime DueDate { get; set; }
+    }
+}
