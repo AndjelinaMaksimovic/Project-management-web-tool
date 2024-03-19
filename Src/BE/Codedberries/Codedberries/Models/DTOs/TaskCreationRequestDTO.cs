f9143c8e9af5bfe0e386213cb861e0b1adbad86c
Marko Nenadovic <67-2020@pmf.kg.ac.rs>
2024-03-19 22:05:42 +0100

    #54 dodat fajl TaskService

--- Src/BE/Codedberries/Codedberries/Models/DTOs/TaskCreationRequestDTO.cs
+++ Src/BE/Codedberries/Codedberries/Models/DTOs/TaskCreationRequestDTO.cs
@@ -7,10 +7,11 @@ namespace Codedberries.Models.DTOs
         public string Description { get; set; }
         public DateTime DueDate { get; set; }
         public int UserId { get; set; }
-        public StatusEnum Status { get; set; }
-        public PriorityEnum Priority { get; set; }
+        public string Status { get; set; }
+        public string Priority { get; set; }
         public int DifficultyLevel { get; set; }
         public int CategoryId { get; set; }
         public List<int> DependencyIds { get; set; }
+        public int ProjectId { get; set; }
     }
 }
