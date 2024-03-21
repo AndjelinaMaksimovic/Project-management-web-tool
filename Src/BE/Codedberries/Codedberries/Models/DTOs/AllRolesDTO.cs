c290335b579b9477a56e70b487568fcc55181500
uros.aksentijevic <24-2021@pmf.kg.ac.rs>
2024-03-21 13:01:58 +0100

    #62 Azuriran Role kontroler i DTO

--- Src/BE/Codedberries/Codedberries/Models/DTOs/AllRolesDTO.cs
+++ Src/BE/Codedberries/Codedberries/Models/DTOs/AllRolesDTO.cs
@@ -1,7 +1,8 @@
 ﻿namespace Codedberries.Models.DTOs
 {
-    public class AllRolesNamesDTO
+    public class AllRolesDTO
     {
         public List<string> RoleNames { get; set; }
+        public List<int> RolesIds { get; set; }
     }
 }
