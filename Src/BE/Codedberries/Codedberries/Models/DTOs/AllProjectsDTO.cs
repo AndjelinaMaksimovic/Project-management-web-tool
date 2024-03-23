682fa04535e4a292184ebfdf90f04105b40b51b0
Milica Rogic <milica.rog@gmail.com>
2024-03-23 07:41:44 +0100

    #67 Kreiran AllProjectsDTO

---
+++ Src/BE/Codedberries/Codedberries/Models/DTOs/AllProjectsDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace Codedberries.Models.DTOs
+{
+    public class AllProjectsDTO
+    {
+        public List<string> ProjectsNames { get; set; }
+        public List<int> ProjectsIds { get; set; }
+    }
+}
