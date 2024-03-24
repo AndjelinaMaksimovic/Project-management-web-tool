5237d5be6995bd8be9919b0ecdfd12349e91f66d
Strahinja Milenovic <59-2021@pmf.kg.ac.rs>
2024-03-24 15:18:20 +0100

    #83 create "my-tasks" page

--- Src/FE/angular-app/src/app/app.routes.ts
+++ Src/FE/angular-app/src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { RegisterComponent } from './pages/register/register.component';
 import { ErrorComponent } from './pages/error/error.component';
 import { ActivateComponent } from './pages/activate/activate.component';
 import { NewProjectComponent } from './pages/new-project/new-project.component';
+import { MyTasksComponent } from './pages/my-tasks/my-tasks.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
@@ -12,5 +13,6 @@ export const routes: Routes = [
   { path: 'register', component: RegisterComponent },
   { path: 'activate', component: ActivateComponent },
   { path: 'new-project', component: NewProjectComponent },
+  { path: 'my-tasks', component: MyTasksComponent },
   { path: '**', component: ErrorComponent },
 ];
