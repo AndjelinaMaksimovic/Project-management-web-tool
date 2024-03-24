c96c67206605968c51728c30e18e1a33b6ea1fa8
Milica Rogic <milica.rog@gmail.com>
2024-03-24 12:44:50 +0100

    #68 Kreiran TaskUserInfoDTO

---
+++ Src/BE/Codedberries/Codedberries/Models/DTOs/TaskUserInfoDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace Codedberries.Models.DTOs
+{
+    public class TaskUserInfoDTO
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string ProfilePicture { get; set; }
+    }
+}
