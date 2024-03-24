5b4132ab17574e00d7cc07125b2e8d41c5441799
Milica Rogic <milica.rog@gmail.com>
2024-03-24 14:28:49 +0100

    #68 Izmena u TaskFilterParamsDTO

--- Src/BE/Codedberries/Codedberries/Models/DTOs/TaskFilterParamsDTO.cs
+++ Src/BE/Codedberries/Codedberries/Models/DTOs/TaskFilterParamsDTO.cs
@@ -12,5 +12,13 @@
         public DateTime? DueDateAfter { get; set; } 
         public DateTime? DueDateBefore { get; set; } 
         public int? CategoryId { get; set; }
+
+        public bool IsEmpty()
+        {
+            return ProjectId == 0 && !AssignedTo.HasValue && string.IsNullOrEmpty(Status)
+                && string.IsNullOrEmpty(Priority) && !DifficultyLevelGreaterThan.HasValue &&
+                !DifficultyLevelLesserThan.HasValue && !DifficultyLevelEquals.HasValue &&
+                !DueDateAfter.HasValue && !DueDateBefore.HasValue && !CategoryId.HasValue;
+        }
     }
 }
