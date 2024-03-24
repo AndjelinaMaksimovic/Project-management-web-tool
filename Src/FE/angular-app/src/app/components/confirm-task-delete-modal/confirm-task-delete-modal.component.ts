79b0070818571392f45cddce4232de598e0b3119
Strahinja Milenovic <59-2021@pmf.kg.ac.rs>
2024-03-24 11:19:23 +0100

    #80 implement delete task modal frame

--- Src/FE/angular-app/src/app/components/confirm-task-delete-modal/confirm-task-delete-modal.component.ts
+++ Src/FE/angular-app/src/app/components/confirm-task-delete-modal/confirm-task-delete-modal.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { MaterialModule } from '../../material/material.module';
 
 @Component({
   selector: 'app-confirm-task-delete-modal',
   standalone: true,
-  imports: [],
+  imports: [MaterialModule],
   templateUrl: './confirm-task-delete-modal.component.html',
   styleUrl: './confirm-task-delete-modal.component.css'
 })
