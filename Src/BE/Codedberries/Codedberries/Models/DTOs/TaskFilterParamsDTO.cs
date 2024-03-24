16b601770595c9b04da93bef859b128058eb3a9b
Milica Rogic <milica.rog@gmail.com>
2024-03-24 12:39:08 +0100

    #68 Kreiran TaskFilterParamsDTO

---
+++ Src/BE/Codedberries/Codedberries/Models/DTOs/TaskFilterParamsDTO.cs
@@ -0,0 +1,14 @@
+﻿namespace Codedberries.Models.DTOs
+{
+    public class TaskFilterParamsDTO
+    {
+        public int ProjectId { get; set; } // required
+        public int? AssignedTo { get; set; } 
+        public string Status { get; set; } 
+        public int? Priority { get; set; } 
+        public int? DifficultyLevel { get; set; } 
+        public DateTime? DueDateAfter { get; set; } 
+        public DateTime? DueDateBefore { get; set; } 
+        public int? CategoryId { get; set; }
+    }
+}
