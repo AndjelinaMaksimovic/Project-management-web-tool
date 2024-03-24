5380015bbe957fa0016636cad2bb923a8922949b
Milica Rogic <milica.rog@gmail.com>
2024-03-24 13:19:20 +0100

    #68 Mala ispravka u TaskFilterDTO

--- Src/BE/Codedberries/Codedberries/Models/DTOs/TaskFilterParamsDTO.cs
+++ Src/BE/Codedberries/Codedberries/Models/DTOs/TaskFilterParamsDTO.cs
@@ -5,7 +5,7 @@
         public int ProjectId { get; set; } // required
         public int? AssignedTo { get; set; } 
         public string Status { get; set; } 
-        public int? Priority { get; set; } 
+        public string? Priority { get; set; } 
         public int? DifficultyLevel { get; set; } 
         public DateTime? DueDateAfter { get; set; } 
         public DateTime? DueDateBefore { get; set; } 
