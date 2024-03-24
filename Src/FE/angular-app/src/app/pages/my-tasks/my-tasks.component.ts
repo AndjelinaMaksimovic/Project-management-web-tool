d02b4269e58c6851a97bf4ff54345c7026849ecd
Strahinja Milenovic <59-2021@pmf.kg.ac.rs>
2024-03-24 15:39:56 +0100

    #83 add search tasks field and create task button

--- Src/FE/angular-app/src/app/pages/my-tasks/my-tasks.component.ts
+++ Src/FE/angular-app/src/app/pages/my-tasks/my-tasks.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { MaterialModule } from '../../material/material.module';
+import { ClearableInputComponent } from '../../components/clearable-input/clearable-input.component';
 
 @Component({
   selector: 'app-my-tasks',
   standalone: true,
-  imports: [],
+  imports: [MaterialModule, ClearableInputComponent],
   templateUrl: './my-tasks.component.html',
   styleUrl: './my-tasks.component.css'
 })
