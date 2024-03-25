20a3bae6742b3a9f0d70043c13151298a7073fa5
Strahinja Milenovic <59-2021@pmf.kg.ac.rs>
2024-03-25 17:34:26 +0100

    #78 add tasks to task table component

--- Src/FE/angular-app/src/app/components/tasks-table/tasks-table.component.ts
+++ Src/FE/angular-app/src/app/components/tasks-table/tasks-table.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { TaskCardComponent } from '../task-card/task-card.component';
 
 @Component({
   selector: 'app-tasks-table',
   standalone: true,
-  imports: [],
+  imports: [TaskCardComponent],
   templateUrl: './tasks-table.component.html',
   styleUrl: './tasks-table.component.css'
 })
 export class TasksTableComponent {
-
+  tasks = [
+    { name: 'Task1', category: 'finance', priority: 'Low', status: 'Finished' },
+    { name: 'Task2', category: 'finance', priority: 'High', status: 'Active' },
+    { name: 'Task 3', category: 'finance', priority: 'High', status: 'Finished' },
+  ] as const;
 }
