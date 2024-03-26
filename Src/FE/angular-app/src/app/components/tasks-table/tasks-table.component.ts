16534da6813094a91da3f6d468be80233cddb235
Strahinja Milenovic <59-2021@pmf.kg.ac.rs>
2024-03-26 10:19:59 +0100

    #78 make tasks table accept input tasks

--- Src/FE/angular-app/src/app/components/tasks-table/tasks-table.component.ts
+++ Src/FE/angular-app/src/app/components/tasks-table/tasks-table.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { TaskCardComponent } from '../task-card/task-card.component';
 
 @Component({
@@ -9,7 +9,7 @@ import { TaskCardComponent } from '../task-card/task-card.component';
   styleUrl: './tasks-table.component.css'
 })
 export class TasksTableComponent {
-  tasks = [
+  @Input() tasks = [
     { title: 'Task1', category: 'finance', priority: 'Low', status: 'Finished', date: new Date("2023"), id: 1 },
     { title: 'Task2', category: 'finance', priority: 'High', status: 'Active', date: new Date("2023"), id: 2 },
     { title: 'Task 3', category: 'finance', priority: 'High', status: 'Finished', date: new Date("2023"), id: 3 },
