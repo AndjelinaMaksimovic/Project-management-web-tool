dc3c6777122d0f60f1dd9d26f2448586da58f737
Strahinja Milenovic <59-2021@pmf.kg.ac.rs>
2024-03-26 12:07:36 +0100

    #79 task card dynamic priority color

--- Src/FE/angular-app/src/app/components/kanban-task-card/kanban-task-card.component.ts
+++ Src/FE/angular-app/src/app/components/kanban-task-card/kanban-task-card.component.ts
@@ -5,7 +5,7 @@ import { Component, Input } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './kanban-task-card.component.html',
-  styleUrl: './kanban-task-card.component.css'
+  styleUrl: './kanban-task-card.component.css',
 })
 export class KanbanTaskCardComponent {
   @Input() task!: Readonly<{
@@ -15,5 +15,22 @@ export class KanbanTaskCardComponent {
     status: 'Finished' | 'Active' | 'Past Due';
     date: Date;
     id: number;
-  }>
+  }>;
+
+  get priorityColor() {
+    const priorityColorMap = {
+      High: 'warn',
+      Medium: 'mid',
+      Low: 'neutral',
+    } as const;
+    return priorityColorMap[this.task.priority];
+  }
+  get statusColor() {
+    const priorityColorMap = {
+      Finished: 'good',
+      Active: 'mid',
+      'Past Due': 'warn',
+    } as const;
+    return priorityColorMap[this.task.status];
+  }
 }
