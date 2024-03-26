911137b10cc5f7fdca21e1b1b614cb6914029bdc
luka.zecevic <44-2020@pmf.kg.ac.rs>
2024-03-26 20:50:11 +0100

    #77 Uklonjena biblioteka jer pravimo novu

---
+++ Src/FE/angular-app/src/app/components/gantt/gantt.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-gantt',
+  standalone: true,
+  imports: [],
+  templateUrl: './gantt.component.html',
+  styleUrl: './gantt.component.css'
+})
+
+export class GanttComponent implements OnInit{
+  // @Input() items?: Items
+
+  ngOnInit(): void {
+
+  }
+
+}
