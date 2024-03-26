e20d8adedef49b7b2d6c1fc9565aabdb805ee89b
Strahinja Milenovic <59-2021@pmf.kg.ac.rs>
2024-03-26 10:49:31 +0100

    #78 update task table input format

--- Src/FE/angular-app/src/app/components/tasks-table/tasks-table.component.ts
+++ Src/FE/angular-app/src/app/components/tasks-table/tasks-table.component.ts
@@ -9,9 +9,12 @@ import { TaskCardComponent } from '../task-card/task-card.component';
   styleUrl: './tasks-table.component.css'
 })
 export class TasksTableComponent {
-  @Input() tasks = [
-    { title: 'Task1', category: 'finance', priority: 'Low', status: 'Finished', date: new Date("2023"), id: 1 },
-    { title: 'Task2', category: 'finance', priority: 'High', status: 'Active', date: new Date("2023"), id: 2 },
-    { title: 'Task 3', category: 'finance', priority: 'High', status: 'Finished', date: new Date("2023"), id: 3 },
-  ] as const;
+  @Input() tasks!: Readonly<{
+    title: string;
+    priority: 'High' | 'Medium' | 'Low';
+    category: string;
+    status: 'Finished' | 'Active' | 'Past Due';
+    date: Date;
+    id: number;
+  }[]>;
 }
