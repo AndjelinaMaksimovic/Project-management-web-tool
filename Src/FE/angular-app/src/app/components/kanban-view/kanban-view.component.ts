babf41d305c515726e8d8f89780648553e9fc847
Strahinja Milenovic <59-2021@pmf.kg.ac.rs>
2024-03-26 13:06:34 +0100

    #79 kanban view: filter rendered items by status

--- Src/FE/angular-app/src/app/components/kanban-view/kanban-view.component.ts
+++ Src/FE/angular-app/src/app/components/kanban-view/kanban-view.component.ts
@@ -18,5 +18,10 @@ export class KanbanViewComponent {
     id: number;
   }[]>;
 
-  statuses = ["Active", "In Progress", "Pending", "Completed"];
+  statuses = ['Finished', 'Active', 'Past Due'] as const;
+  // statuses = ["Active", "In Progress", "Pending", "Completed"] as const;
+
+  getTasksOfStatus(status: (typeof this.tasks)[number]["status"]){
+    return this.tasks.filter((t) => t.status === status);
+  }
 }
