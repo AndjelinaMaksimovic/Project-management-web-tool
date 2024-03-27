b4cea7f2e143bcf36b8d55620bed6b94c8bbb1fe
Strahinja Milenovic <59-2021@pmf.kg.ac.rs>
2024-03-27 08:23:11 +0100

    #90 import views in tasks page; render kanban view sample

--- Src/FE/angular-app/src/app/pages/my-tasks/my-tasks.component.ts
+++ Src/FE/angular-app/src/app/pages/my-tasks/my-tasks.component.ts
@@ -1,17 +1,21 @@
 import { Component } from '@angular/core';
 import { MaterialModule } from '../../material/material.module';
 import { ClearableInputComponent } from '../../components/clearable-input/clearable-input.component';
+import { KanbanViewComponent } from '../../components/kanban-view/kanban-view.component';
+import { TasksTableComponent } from '../../components/tasks-table/tasks-table.component';
 
 @Component({
   selector: 'app-my-tasks',
   standalone: true,
-  imports: [MaterialModule, ClearableInputComponent],
+  imports: [MaterialModule, ClearableInputComponent, KanbanViewComponent, TasksTableComponent],
   templateUrl: './my-tasks.component.html',
   styleUrl: './my-tasks.component.css',
 })
 export class MyTasksComponent {
   tasks = [
-    { name: 'task1', category: 'finance', priority: 'low', status: 'active' },
-    { name: 'task2', category: 'finance', priority: 'low', status: 'active' },
-  ];
+    { title: 'Task name 1', category: 'Finance', priority: 'Low', status: 'Active', id: 1, date: new Date("2024") },
+    { title: 'Task 2', category: 'Finance', priority: 'Low', status: 'Active', id: 1, date: new Date("2024") },
+    { title: 'Task 3', category: 'Marketing', priority: 'High', status: 'Past Due', id: 1, date: new Date("2024") },
+    { title: 'Task 4', category: 'Finance', priority: 'High', status: 'Closed', id: 1, date: new Date("2024") },
+  ] as const;
 }
