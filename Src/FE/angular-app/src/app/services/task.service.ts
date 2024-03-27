7da821fdbd68c36190180bfd35c3f12c2930ba11
Strahinja Milenovic <59-2021@pmf.kg.ac.rs>
2024-03-27 09:18:09 +0100

    #90 add task service

---
+++ Src/FE/angular-app/src/app/services/task.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TaskService {
+
+  private _tasks = [
+    { title: 'Task name 1', category: 'Finance', priority: 'Low', status: 'Active', id: 1, date: new Date("2024") },
+    { title: 'Task 2', category: 'Finance', priority: 'Low', status: 'Active', id: 1, date: new Date("2024") },
+    { title: 'Task 3', category: 'Marketing', priority: 'High', status: 'Past Due', id: 1, date: new Date("2024") },
+    { title: 'Task 4', category: 'Finance', priority: 'High', status: 'Closed', id: 1, date: new Date("2024") },
+    { title: 'Task name 4', category: 'Finance', priority: 'Medium', status: 'Review', id: 1, date: new Date("2024") },
+    { title: 'Task name 5', category: 'Finance', priority: 'Medium', status: 'Review', id: 1, date: new Date("2024") },
+  ] as const;
+
+  getTasks(){
+    return this._tasks
+  }
+
+  constructor() { }
+}
