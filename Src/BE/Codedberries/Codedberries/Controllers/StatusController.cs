f4ae444f40aa611e91c256baad4831e353c37ca6
Milica Rogic <milica.rog@gmail.com>
2024-03-28 09:15:01 +0100

    #95 Kreiran StatusController i createStatus endpoint

---
+++ Src/BE/Codedberries/Codedberries/Controllers/StatusController.cs
@@ -0,0 +1,39 @@
+﻿using Codedberries.Helpers;
+using Codedberries.Models.DTOs;
+using Codedberries.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Codedberries.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StatusController : ControllerBase
+    {
+        private readonly StatusService _statusService;
+
+        public StatusController(StatusService statusService)
+        {
+            _statusService = statusService;
+        }
+
+        [HttpPost("createStatus")]
+        public async System.Threading.Tasks.Task<IActionResult> CreateStatus([FromBody] StatusCreationDTO statusDTO)
+        {
+            try
+            {
+                await _statusService.CreateStatus(HttpContext, statusDTO);
+
+                return Ok("Status successfully created.");
+            }
+            catch (UnauthorizedAccessException ex)
+            {
+                return Unauthorized(new ErrorMsg(ex.Message));
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, new ErrorMsg($"An error occurred while creating the status: {ex.Message}"));
+            }
+        }
+    }
+}
