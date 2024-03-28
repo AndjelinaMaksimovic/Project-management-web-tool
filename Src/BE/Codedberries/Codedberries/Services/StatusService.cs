a431101fde5791fb7bf1dd1f7038bbe971324850
Milica Rogic <milica.rog@gmail.com>
2024-03-28 09:11:29 +0100

    #95 Kreiran StatusService i CreateStatus servis

---
+++ Src/BE/Codedberries/Codedberries/Services/StatusService.cs
@@ -0,0 +1,32 @@
+﻿using Codedberries.Models.DTOs;
+using Codedberries.Models;
+
+namespace Codedberries.Services
+{
+    public class StatusService
+    {
+        private readonly AppDatabaseContext _databaseContext;
+        private readonly AuthorizationService _authorizationService;
+
+        public StatusService(AppDatabaseContext dbContext, AuthorizationService authorizationService)
+        {
+            _databaseContext = dbContext;
+            _authorizationService = authorizationService;
+        }
+
+        public async System.Threading.Tasks.Task CreateStatus(HttpContext httpContext, StatusCreationDTO statusDTO)
+        {
+            var userId = _authorizationService.GetUserIdFromSession(httpContext);
+
+            if (userId == null)
+            {
+                throw new UnauthorizedAccessException("Invalid session!");
+            }
+
+            var newStatus = new Models.Status(statusDTO.Name);
+
+            _databaseContext.Statuses.Add(newStatus);
+            await _databaseContext.SaveChangesAsync();
+        }
+    }
+}
