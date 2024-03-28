d596c93ca2537823cc5d453dc615fde324811af4
luka.zecevic <44-2020@pmf.kg.ac.rs>
2024-03-28 13:23:43 +0100

    #93 quickfix

--- Src/FE/angular-app/src/environments/environment.development.ts
+++ Src/FE/angular-app/src/environments/environment.development.ts
@@ -1,3 +1,10 @@
+import { HttpHeaders } from "@angular/common/http";
+
 export const environment = {
-    apiUrl: 'https://localhost:7167/api'
+    apiUrl: 'https://localhost:7167/api',
+    httpOptions: {
+        headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+        withCredentials: true,
+        observe: 'response' as 'response'
+    }
 };
