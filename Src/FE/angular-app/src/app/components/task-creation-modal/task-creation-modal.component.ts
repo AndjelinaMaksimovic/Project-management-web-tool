96fe247cb39a9cd29446da5e5df7d2c5c2f34184
Strahinja Milenovic <59-2021@pmf.kg.ac.rs>
2024-03-28 08:52:00 +0100

    #91 add task creation modal component

---
+++ Src/FE/angular-app/src/app/components/task-creation-modal/task-creation-modal.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-task-creation-modal',
+  standalone: true,
+  imports: [],
+  templateUrl: './task-creation-modal.component.html',
+  styleUrl: './task-creation-modal.component.css'
+})
+export class TaskCreationModalComponent {
+
+}
