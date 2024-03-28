0632576924ccc7a70158b6b11b91b3c970a1f09d
Strahinja Milenovic <59-2021@pmf.kg.ac.rs>
2024-03-28 10:42:42 +0100

    #91 add category to task creation

--- Src/FE/angular-app/src/app/components/task-creation-modal/task-creation-modal.component.ts
+++ Src/FE/angular-app/src/app/components/task-creation-modal/task-creation-modal.component.ts
@@ -32,11 +32,17 @@ export class TaskCreationModalComponent {
   description: string | null = null;
   date: string | null = null;
   priority: string | null = null;
+  category: string | null = null;
 
   priorities = [
     { value: 'Low', viewValue: 'Low' },
     { value: 'Medium', viewValue: 'Medium' },
     { value: 'High', viewValue: 'High' },
+  ];
+  categories = [
+    { value: 'Finance', viewValue: 'Finance' },
+    { value: 'Marketing', viewValue: 'Marketing' },
+    { value: 'Development', viewValue: 'Development' },
   ];
 
   constructor(
@@ -45,8 +51,14 @@ export class TaskCreationModalComponent {
   ) {}
 
   async createTask() {
-    if (!this.title || !this.date || !this.priority || !this.description) {
-      this.errorMessage = "Please provide all required fields";
+    if (
+      !this.title ||
+      !this.date ||
+      !this.priority ||
+      !this.description ||
+      !this.category
+    ) {
+      this.errorMessage = 'Please provide all required fields';
       return;
     }
     await this.taskService.createTask(
@@ -54,7 +66,7 @@ export class TaskCreationModalComponent {
         title: this.title,
         description: this.description,
         date: new Date(this.date),
-        category: 'Finance',
+        category: this.category,
         priority: this.priority as 'Low' | 'High' | 'Medium',
         status: 'Active',
       },
@@ -63,68 +75,3 @@ export class TaskCreationModalComponent {
     this.dialogRef.close();
   }
 }
-
-// import { Component } from '@angular/core';
-// import { MaterialModule } from '../../material/material.module';
-// import { AuthService } from '../../services/auth.service';
-// import { FormsModule } from '@angular/forms';
-// import { CommonModule } from '@angular/common';
-// import { ActivatedRoute, Router } from '@angular/router';
-// import { ClearableInputComponent } from '../../components/clearable-input/clearable-input.component';
-// import { EmailFieldComponent } from '../../components/email-field/email-field.component';
-// import { SelectComponent } from '../../components/select/select.component';
-// import { RolesService } from '../../services/roles.service';
-// @Component({
-//   selector: 'app-invite-modal',
-//   standalone: true,
-//   imports: [MaterialModule, FormsModule, CommonModule, ClearableInputComponent, EmailFieldComponent, MaterialModule, SelectComponent],
-//   templateUrl: './invite-modal.component.html',
-//   styleUrl: './invite-modal.component.css'
-// })
-// export class InviteModalComponent {
-//   /**
-//    * placeholder for API values
-//    */
-//   roles: {value: string, viewValue: string}[] = [];
-//   email: string = '';
-//   firstName: string = '';
-//   lastName: string = '';
-//   role: string | null = null;
-//   errorMessage: string | null = null;
-
-//   constructor(
-//     private authService: AuthService,
-//     private rolesService: RolesService,
-//     private router: Router,
-//     private route: ActivatedRoute
-//   ) {}
-//   async ngOnInit(){
-//     const roles = await this.rolesService.getAllRoles();
-//     if(!roles) return;
-//     this.roles = roles.map(role => ({value: role.id.toString(), viewValue: role.roleName}))
-//   }
-//   async register() {
-//     // check email
-//     if (this.email === undefined) {
-//       this.errorMessage = 'no email passed';
-//       return;
-//     }
-//     if (this.role === null) {
-//       this.errorMessage = 'please select a role';
-//       return;
-//     }
-//     const res = await this.authService.register(
-//       this.email,
-//       this.firstName,
-//       this.lastName,
-//       this.role,
-//     );
-//     // if registration fails return
-//     if (!res) {
-//       this.errorMessage = 'registration failed';
-//       return;
-//     }
-//     // on successful registration, redirect to home?
-//     this.router.navigate(['/login']);
-//   }
-// }
