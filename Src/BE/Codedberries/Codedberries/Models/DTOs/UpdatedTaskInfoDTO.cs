de5210b2b13424c6f7e2fb773dc5560f926e5937
Milica Rogic <milica.rog@gmail.com>
2024-03-31 11:57:09 +0200

    #102 Kreiran UpdatedTaskInfoDTO

---
+++ Src/BE/Codedberries/Codedberries/Models/DTOs/UpdatedTaskInfoDTO.cs
@@ -0,0 +1,14 @@
+﻿namespace Codedberries.Models.DTOs
+{
+    public class UpdatedTaskInfoDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int CategoryId { get; set; }
+        public int PriorityId { get; set; }
+        public int StatusId { get; set; }
+        public DateTime DueDate { get; set; }
+        public List<UserDTO> AssignedUsers { get; set; }
+    }
+}
