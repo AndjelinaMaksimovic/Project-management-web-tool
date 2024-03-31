f58c53fe87f54c849d8cef8ec87be64c50260695
Milica Rogic <milica.rog@gmail.com>
2024-03-31 13:01:36 +0200

    #102 Izmena kod TaskUpdateRequestDTO

--- Src/BE/Codedberries/Codedberries/Models/DTOs/TaskUpdateRequestDTO.cs
+++ Src/BE/Codedberries/Codedberries/Models/DTOs/TaskUpdateRequestDTO.cs
@@ -3,8 +3,8 @@
     public class TaskUpdateRequestDTO
     {
         public int TaskId { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
         public int? CategoryId { get; set; }
         public int? PriorityId { get; set; }
         public int? StatusId { get; set; }
