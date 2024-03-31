bc1519dc15b26c0d2d2497fcb85a6b641ade8dd2
Marko Nenadovic <67-2020@pmf.kg.ac.rs>
2024-03-31 20:13:20 +0200

    #100 migracija baze

---
+++ Src/BE/Codedberries/Codedberries/Migrations/20240331171354_UpdateTaskV6.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Codedberries.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateTaskV6 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Archieved",
+                table: "Tasks",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Archieved",
+                table: "Tasks");
+        }
+    }
+}
