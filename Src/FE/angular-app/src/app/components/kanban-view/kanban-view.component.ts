e542bb327f1768fe7fc406215fca4a7bc154a4ba
Strahinja Milenovic <59-2021@pmf.kg.ac.rs>
2024-04-01 09:12:35 +0200

    #103 make kanban responsive

--- Src/FE/angular-app/src/app/components/kanban-view/kanban-view.component.ts
+++ Src/FE/angular-app/src/app/components/kanban-view/kanban-view.component.ts
@@ -9,16 +9,25 @@ import {
 } from '@angular/cdk/drag-drop';
 import { KanbanTaskCardComponent } from '../kanban-task-card/kanban-task-card.component';
 import { Task, TaskService } from '../../services/task.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-kanban-view',
   standalone: true,
-  imports: [KanbanTaskCardComponent, CdkDropListGroup, CdkDropList, CdkDrag ],
+  imports: [KanbanTaskCardComponent, CdkDropListGroup, CdkDropList, CdkDrag, CommonModule ],
   templateUrl: './kanban-view.component.html',
   styleUrl: './kanban-view.component.css',
 })
 export class KanbanViewComponent {
   constructor(private taskService: TaskService){}
+  mobile: boolean = false;
+  ngOnInit() {
+    if (window.screen.width < 600) { // 768px portrait
+      this.mobile = true;
+    } else {
+      this.mobile = false;
+    }
+  }
   _tasks: Task[] = [];
   @Input()
   public set tasks(
