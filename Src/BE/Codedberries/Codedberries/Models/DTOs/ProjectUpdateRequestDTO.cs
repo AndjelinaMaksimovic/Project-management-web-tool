6cb88ef0c50d40fd8e1f7967e4a909cca80b107a
Marko Nenadovic <67-2020@pmf.kg.ac.rs>
2024-04-01 20:57:13 +0200

    #105 kreiran ProjectUpdateRequestDTO

---
+++ Src/BE/Codedberries/Codedberries/Models/DTOs/ProjectUpdateRequestDTO.cs
@@ -0,0 +1,25 @@
+﻿using System.Xml.Linq;
+
+namespace Codedberries.Models.DTOs
+{
+    public class ProjectUpdateRequestDTO
+    {
+        public int ProjectId { get; set; }
+        public string? ProjectName { get; set; }
+        public string? ProjectDescription { get; set; }
+        public List<UserDTO>? Users { get; set; }
+        public DateTime? DueDate { get; set; }
+        public DateTime? StartDate { get; set; }
+
+        public bool IsEmpty()
+        {
+            return string.IsNullOrEmpty(ProjectName) &&
+                   string.IsNullOrEmpty(ProjectDescription) &&
+                   Users == null &&
+                   DueDate == null &&
+                   StartDate == null;
+                   
+
+        }
+    }
+}
