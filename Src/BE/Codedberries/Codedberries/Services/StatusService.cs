3e377145ef4b42300321cb6f534c21553508454c
Milica Rogic <milica.rog@gmail.com>
2024-04-01 21:05:23 +0200

    #106 Provera uloge i permisija u DeleteStatusesByProjectId servisu

--- Src/BE/Codedberries/Codedberries/Services/StatusService.cs
+++ Src/BE/Codedberries/Codedberries/Services/StatusService.cs
@@ -108,6 +108,18 @@ namespace Codedberries.Services
             {
                 throw new UnauthorizedAccessException("User does not have any role assigned!");
             }
+
+            var userRole = _databaseContext.Roles.FirstOrDefault(r => r.Id == user.RoleId);
+
+            if (userRole == null)
+            {
+                throw new UnauthorizedAccessException("User role not found!");
+            }
+
+            if (userRole.CanDeleteProject == false || userRole.CanRemoveTask == false)
+            {
+                throw new UnauthorizedAccessException("User does not have permission to delete status!");
+            }
         }
     }
 }
