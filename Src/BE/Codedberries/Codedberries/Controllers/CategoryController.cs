b2337e1c09eac4d6719216d15515e35920cd9e9d
Milica Rogic <milica.rog@gmail.com>
2024-04-02 10:55:10 +0200

    #107 Kreiran CategoryController

---
+++ Src/BE/Codedberries/Codedberries/Controllers/CategoryController.cs
@@ -0,0 +1,18 @@
+﻿using Codedberries.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Codedberries.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        private readonly CategoryService _categoryService;
+
+        public CategoryController(CategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+    }
+}
