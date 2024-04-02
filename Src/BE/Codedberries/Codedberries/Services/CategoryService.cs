e713c8690d6a82661f8275c7541dd7e744439fb6
Milica Rogic <milica.rog@gmail.com>
2024-04-02 10:40:38 +0200

    #107 Kreiran CategoryService

---
+++ Src/BE/Codedberries/Codedberries/Services/CategoryService.cs
@@ -0,0 +1,14 @@
+﻿namespace Codedberries.Services
+{
+    public class CategoryService
+    {
+        private readonly AppDatabaseContext _context;
+        private readonly AuthorizationService _authorizationService;
+
+        public CategoryService(AppDatabaseContext context, AuthorizationService authorizationService)
+        {
+            _context = context;
+            _authorizationService = authorizationService;
+        }
+    }
+}
