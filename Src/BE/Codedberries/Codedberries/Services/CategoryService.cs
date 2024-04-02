d6e13206379ca058fed8c0fd605560388efde517
Milica Rogic <milica.rog@gmail.com>
2024-04-02 10:42:45 +0200

    #107 Kreiran servis CreateNewCategory i provera sesije

--- Src/BE/Codedberries/Codedberries/Services/CategoryService.cs
+++ Src/BE/Codedberries/Codedberries/Services/CategoryService.cs
@@ -1,4 +1,7 @@
-﻿namespace Codedberries.Services
+﻿using Codedberries.Models.DTOs;
+using Microsoft.AspNetCore.Http;
+
+namespace Codedberries.Services
 {
     public class CategoryService
     {
@@ -9,6 +12,18 @@
         {
             _context = context;
             _authorizationService = authorizationService;
+        }
+
+        public bool CreateNewCategory(HttpContext httpContext, CreateCategoryDTO categoryDTO)
+        {
+            var userId = _authorizationService.GetUserIdFromSession(httpContext);
+
+            if (userId == null)
+            {
+                throw new UnauthorizedAccessException("Invalid session!");
+            }
+
+            return true;
         }
     }
 }
