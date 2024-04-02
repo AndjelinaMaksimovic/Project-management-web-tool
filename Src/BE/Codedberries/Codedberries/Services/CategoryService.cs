98b838cd4cf259837df9ad66f9345793dcf18102
Milica Rogic <milica.rog@gmail.com>
2024-04-02 11:30:24 +0200

    #107 Kreiran servis GetAllProjectCategories

--- Src/BE/Codedberries/Codedberries/Services/CategoryService.cs
+++ Src/BE/Codedberries/Codedberries/Services/CategoryService.cs
@@ -1,6 +1,8 @@
 ﻿using Codedberries.Models;
 using Codedberries.Models.DTOs;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using System.Net.Http;
 
 namespace Codedberries.Services
 {
@@ -69,6 +71,47 @@ namespace Codedberries.Services
             
             _databaseContext.Categories.Add(newCategory);
             await _databaseContext.SaveChangesAsync();
+        }
+
+        public async Task<List<CategoryDTO>> GetAllProjectCategories(HttpContext httpContext, ProjectIdDTO request)
+        {
+            var userId = _authorizationService.GetUserIdFromSession(httpContext);
+
+            if (userId == null)
+            {
+                throw new UnauthorizedAccessException("Invalid session!");
+            }
+
+            var user = _databaseContext.Users.FirstOrDefault(u => u.Id == userId);
+
+            if (user == null)
+            {
+                throw new UnauthorizedAccessException("User not found!");
+            }
+
+            if (user.RoleId == null)
+            {
+                throw new UnauthorizedAccessException("User does not have any role assigned!");
+            }
+
+            var userRole = _databaseContext.Roles.FirstOrDefault(r => r.Id == user.RoleId);
+
+            if (userRole == null)
+            {
+                throw new UnauthorizedAccessException("User role not found!");
+            }
+
+            var categories = await _databaseContext.Categories
+                .Where(c => c.ProjectId == request.ProjectId)
+                .Select(c => new CategoryDTO { Id = c.Id, Name = c.Name })
+                .ToListAsync();
+
+            if (categories == null || !categories.Any())
+            {
+                throw new InvalidOperationException("No categories found for the specified project.");
+            }
+
+            return categories;
         }
     }
 }
