b4d3e36591b67721c3b1d1fe8b71414c4b7c17c3
Milica Rogic <milica.rog@gmail.com>
2024-04-02 11:19:27 +0200

    #107 Kreiran ProjectIdDTO

---
+++ Src/BE/Codedberries/Codedberries/Models/DTOs/ProjectIdDTO.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Codedberries.Models.DTOs
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProjectIdDTO : ControllerBase
+    {
+        public int ProjectId { get; set; }
+    }
+}
