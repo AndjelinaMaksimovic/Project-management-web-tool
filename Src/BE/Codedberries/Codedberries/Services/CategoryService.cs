3bbb80694081a4d6c8c1ca063a493470abffc620
Milica Rogic <milica.rog@gmail.com>
2024-04-02 10:46:40 +0200

    #107 Dodata provera korisnika, uloge i permisije za kreiranje unutar CreateNewCategory servisa

--- Src/BE/Codedberries/Codedberries/Services/CategoryService.cs
+++ Src/BE/Codedberries/Codedberries/Services/CategoryService.cs
@@ -5,12 +5,12 @@ namespace Codedberries.Services
 {
     public class CategoryService
     {
-        private readonly AppDatabaseContext _context;
+        private readonly AppDatabaseContext _databaseContext;
         private readonly AuthorizationService _authorizationService;
 
-        public CategoryService(AppDatabaseContext context, AuthorizationService authorizationService)
+        public CategoryService(AppDatabaseContext databaseContext, AuthorizationService authorizationService)
         {
-            _context = context;
+            _databaseContext = databaseContext;
             _authorizationService = authorizationService;
         }
 
@@ -21,6 +21,30 @@ namespace Codedberries.Services
             if (userId == null)
             {
                 throw new UnauthorizedAccessException("Invalid session!");
+            }
+
+            var user = _databaseContext.Users.FirstOrDefault(u => u.Id == userId);
+
+            if (user == null)
+            {
+                throw new UnauthorizedAccessException("User not found!");
+            }
+
+            if (user.RoleId == null)
+            {
+                throw new UnauthorizedAccessException("User does not have any role assigned!");
+            }
+
+            var userRole = _databaseContext.Roles.FirstOrDefault(r => r.Id == user.RoleId);
+
+            if (userRole == null)
+            {
+                throw new UnauthorizedAccessException("User role not found!");
+            }
+
+            if (userRole.CanCreateProject == false)
+            {
+                throw new UnauthorizedAccessException("User does not have permission to create new category!");
             }
 
             return true;
