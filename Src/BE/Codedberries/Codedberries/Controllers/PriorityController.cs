25f118112935a19290e34abc2881e5c0e51cc5a9
Milica Rogic <milica.rog@gmail.com>
2024-04-03 07:55:49 +0200

    #108 Kreiran PriorityController

---
+++ Src/BE/Codedberries/Codedberries/Controllers/PriorityController.cs
@@ -0,0 +1,18 @@
+﻿using Codedberries.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Codedberries.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PriorityController : ControllerBase
+    {
+        private readonly PriorityService _priorityService;
+
+        public PriorityController(PriorityService priorityService)
+        {
+            _priorityService = priorityService;
+        }
+    }
+}
