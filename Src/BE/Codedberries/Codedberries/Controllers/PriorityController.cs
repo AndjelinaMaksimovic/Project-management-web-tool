3c286ebc3bc89bb3cd1349ede661ed3a2284b9fa
Milica Rogic <milica.rog@gmail.com>
2024-04-03 07:59:23 +0200

    #108 Kreiran enpoint allPriorities u PriorityController-u

--- Src/BE/Codedberries/Codedberries/Controllers/PriorityController.cs
+++ Src/BE/Codedberries/Codedberries/Controllers/PriorityController.cs
@@ -1,4 +1,5 @@
-﻿using Codedberries.Services;
+﻿using Codedberries.Helpers;
+using Codedberries.Services;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -13,6 +14,29 @@ namespace Codedberries.Controllers
         public PriorityController(PriorityService priorityService)
         {
             _priorityService = priorityService;
+        }
+
+        [HttpGet("allPriorities")]
+        public async Task<IActionResult> GetAllPriorities()
+        {
+            try
+            {
+                var priorities = await _priorityService.GetAllPriorities(HttpContext);
+
+                return Ok(priorities);
+            }
+            catch (UnauthorizedAccessException ex)
+            {
+                return Unauthorized(new ErrorMsg(ex.Message));
+            }
+            catch (InvalidOperationException ex)
+            {
+                return NotFound(new ErrorMsg(ex.Message));
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new ErrorMsg($"An error occurred: {ex.Message}"));
+            }
         }
     }
 }
