a2cccc1f6a29efeef53e42420c80bb9e6087a3b9
Marko Nenadovic <67-2020@pmf.kg.ac.rs>
2024-04-04 00:06:38 +0200

    #115 azuriran TaskCreationRequestDTO

--- Src/BE/Codedberries/Codedberries/Models/DTOs/TaskCreationRequestDTO.cs
+++ Src/BE/Codedberries/Codedberries/Models/DTOs/TaskCreationRequestDTO.cs
@@ -7,7 +7,7 @@ namespace Codedberries.Models.DTOs
         public string Name { get; set; }
         public string Description { get; set; }
         public DateTime DueDate { get; set; }
-       
+        public DateTime StartDate { get; set; }
         public int StatusId { get; set; }
         public int PriorityId { get; set; }
         public int DifficultyLevel { get; set; }
