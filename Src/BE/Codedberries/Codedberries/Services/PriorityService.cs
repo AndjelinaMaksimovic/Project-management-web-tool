138cf2670419b2c2458f801e81830e1b7ebd88d7
Milica Rogic <milica.rog@gmail.com>
2024-04-03 07:50:24 +0200

    #108 Kreiran PriorityService

---
+++ Src/BE/Codedberries/Codedberries/Services/PriorityService.cs
@@ -0,0 +1,14 @@
+﻿namespace Codedberries.Services
+{
+    public class PriorityService
+    {
+        private readonly AppDatabaseContext _databaseContext;
+        private readonly AuthorizationService _authorizationService;
+
+        public PriorityService(AppDatabaseContext databaseContext, AuthorizationService authorizationService)
+        {
+            _databaseContext = databaseContext;
+            _authorizationService = authorizationService;
+        }
+    }
+}
