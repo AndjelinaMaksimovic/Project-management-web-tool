c78d5cd60f2599cde06b6e19cd6ff96e32031369
uros.aksentijevic <24-2021@pmf.kg.ac.rs>
2024-04-04 13:24:02 +0200

    #118 Sredjivanje bug-a

--- Src/BE/Codedberries/Codedberries/Models/DTOs/UpdatedTaskInfoDTO.cs
+++ Src/BE/Codedberries/Codedberries/Models/DTOs/UpdatedTaskInfoDTO.cs
@@ -6,11 +6,8 @@
         public string Name { get; set; }
         public string Description { get; set; }
         public int CategoryId { get; set; }
-        public string CategoryName { get; set; }
         public int PriorityId { get; set; }
-        public string PriorityName { get; set; }
         public int StatusId { get; set; }
-        public string StatusName { get; set; }
         public DateTime DueDate { get; set; }
         public DateTime StartDate { get; set; }
         public int DifficultyLevel { get; set; }
