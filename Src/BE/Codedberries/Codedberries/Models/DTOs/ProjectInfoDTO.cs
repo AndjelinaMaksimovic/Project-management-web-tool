0738330ff05e50459444e19a8b80c4b86a69d508
Milica Rogic <milica.rog@gmail.com>
2024-04-05 12:58:04 +0200

    #120 Azuriran ProjectInfoDTO

--- Src/BE/Codedberries/Codedberries/Models/DTOs/ProjectInfoDTO.cs
+++ Src/BE/Codedberries/Codedberries/Models/DTOs/ProjectInfoDTO.cs
@@ -6,7 +6,6 @@
         public string Name { get; set; }
         public string Description { get; set; }
         public DateTime DueDate { get; set; }
-        public bool Starred { get; set; }
         public DateTime StartDate { get; set; }
     }
 }
