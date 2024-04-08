9eceaff6b359c48bfe4f142bedf0a27b915b611f
Milica Rogic <milica.rog@gmail.com>
2024-04-08 18:55:52 +0200

    #119 Kreiran model Starred

---
+++ Src/BE/Codedberries/Codedberries/Models/Starred.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Codedberries.Models
+{
+    [Table("Starred")]
+    public class Starred
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [ForeignKey("Project")]
+        public int ProjectId { get; set; }
+        public Project Project { get; set; }
+
+        [Required]
+        [ForeignKey("User")]
+        public int UserId { get; set; }
+        public User User { get; set; }
+
+        public Starred(int projectId, int userId)
+        {
+            ProjectId = projectId;
+            UserId = userId;
+        }
+    }
+}
