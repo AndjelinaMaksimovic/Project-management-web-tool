b6815ad51baf0cd939aa7e570cdda6d319bfb250
uros.aksentijevic <24-2021@pmf.kg.ac.rs>
2024-04-09 00:35:44 +0200

    #116 Dodavanje dinamickih filtera

--- Src/FE/angular-app/src/app/components/filters/filters.component.ts
+++ Src/FE/angular-app/src/app/components/filters/filters.component.ts
@@ -1,12 +1,75 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { MatSelectModule } from '@angular/material/select';
+import { NgFor } from '@angular/common';
+import { MatMenuModule } from '@angular/material/menu';
+import { CommonModule } from '@angular/common';
+import { NgIf } from '@angular/common';
+
+class Item {
+  value: string;
+  name: string;
+
+  constructor(item?: any) {
+    this.value = (item && item.value) || null;
+    this.name = (item && item.name) || null;
+  }
+}
+
+class Filter {
+  value: string;
+  icon: string;
+  name: string;
+  type: string;
+  items: Array<any>;
+  enabled: boolean;
+
+  constructor(filter?: any) {
+    this.value = (filter && filter.value) || null;
+    this.icon = (filter && filter.icon) || null;
+    this.name = (filter && filter.name) || null;
+    this.type = (filter && filter.type) || "";
+    this.items = (filter && filter.items) || [];
+    this.enabled = (filter && filter.enabled) || true;
+  }
+}
 
 @Component({
   selector: 'app-filters',
   standalone: true,
-  imports: [],
+  imports: [ MatSelectModule, NgFor, NgIf, MatMenuModule, CommonModule ],
   templateUrl: './filters.component.html',
   styleUrl: './filters.component.css'
 })
 export class FiltersComponent {
+  @Input() title: string = "";
+  currentFilters: Map<number, boolean> = new Map();
 
+  @Input() allFilters: Filter[] = [
+    // new Filter({ name: 'Start date', icon: 'fa-regular fa-calendar', type: 'date' }),
+    // new Filter({ name: 'Due date', icon: 'fa-solid fa-flag-checkered', type: 'date' }),
+    // new Filter({ name: 'Created by', icon: 'fa-solid fa-user', type: 'select', items: [ new Item({ value: "1", name: "Test" }) ] }),
+    // new Filter({ name: 'Created on', icon: 'fa-regular fa-calendar-plus', type: 'date' }),
+    // new Filter({ name: 'Last modified on', icon: 'fa-regular fa-pen-to-square', type: 'date' }),
+    // new Filter({ name: 'Priority', icon: 'fa-solid fa-up-long', type: 'select', items: [ new Item({ value: "1", name: "Low" }), new Item({ value: "2", name: "Medium" }), new Item({ value: "3", name: "High" }) ] }),
+  ];
+  
+  constructor() {
+    this.allFilters.map((item, index) => ({ ...item, id: index + 1 }));
+  }
+
+  addFilter(filter : Filter, i : number) {
+    this.currentFilters.set(i, true);
+    this.allFilters[i].enabled = false;
+  }
+
+  removeFilter(i : number) {
+    this.currentFilters.delete(i);
+    this.allFilters[i].enabled = true;
+  }
+
+  searchUser(term: string, item: any) {
+    item.name = item.name.replace(',','');
+    term = term.toLocaleLowerCase();
+    return item.name.toLocaleLowerCase().indexOf(term) > -1;
+  }
 }
