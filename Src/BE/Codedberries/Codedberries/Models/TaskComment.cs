e696704e9cc00e990e08819573d9746bc7ecdd34
Marko Nenadovic <67-2020@pmf.kg.ac.rs>
2024-04-09 01:27:38 +0200

    #128 dodat model TaskComment

---
+++ Src/BE/Codedberries/Codedberries/Models/TaskComment.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Codedberries.Models
+{
+    [Table("TaskComments")]
+    public class TaskComment
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int CommentId { get; set; }
+
+        [Required]
+        public string Comment { get; set; }
+
+        [ForeignKey("UserId")]
+        public int UserId { get; set; }
+
+        [ForeignKey("TaskId")]
+        public int TaskId { get; set; }
+
+        public TaskComment(string comment, int userid, int taskid)
+        {
+            Comment = comment;
+            UserId = userid;
+            TaskId = taskid;
+        }
+    }
+}
