1693d6087d2fa4735012feff43762a09db3792c8
Marko Nenadovic <67-2020@pmf.kg.ac.rs>
2024-04-09 18:17:01 +0200

    #130 kreiran model Milestone

---
+++ Src/BE/Codedberries/Codedberries/Models/Milestone.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Codedberries.Models
+{
+    [Table("Milestones")]
+    public class Milestone
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int MilestoneId { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public DateTime MilestoneDate { get; set; }
+
+        [Required]
+        public int ProjectId { get; set; }
+
+        [ForeignKey("ProjectId")]
+        public Project Project { get; set; }
+
+        public Milestone(string name, int projectId, DateTime dateTime)
+        {
+            Name = name;
+            ProjectId = projectId;
+            MilestoneDate = dateTime;
+        }
+    }
+}
