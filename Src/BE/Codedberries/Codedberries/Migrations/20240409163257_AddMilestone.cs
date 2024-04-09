843afd907ddeb90060f8a48f101bbbf60b8c7889
Marko Nenadovic <67-2020@pmf.kg.ac.rs>
2024-04-09 19:34:03 +0200

    #130 izvrsena migracija

---
+++ Src/BE/Codedberries/Codedberries/Migrations/20240409163257_AddMilestone.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Codedberries.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddMilestone : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Milestones",
+                columns: table => new
+                {
+                    MilestoneId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    Date = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    ProjectId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Milestones", x => x.MilestoneId);
+                    table.ForeignKey(
+                        name: "FK_Milestones_Projects_ProjectId",
+                        column: x => x.ProjectId,
+                        principalTable: "Projects",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Milestones_ProjectId",
+                table: "Milestones",
+                column: "ProjectId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Milestones");
+        }
+    }
+}
