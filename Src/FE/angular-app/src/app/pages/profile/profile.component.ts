b572d0cc8885b5e133d8244e793ff8d2c35fcc63
Strahinja Milenovic <59-2021@pmf.kg.ac.rs>
2024-04-10 10:18:46 +0200

    #127 connect profile page with backend

--- Src/FE/angular-app/src/app/pages/profile/profile.component.ts
+++ Src/FE/angular-app/src/app/pages/profile/profile.component.ts
@@ -1,15 +1,23 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
 import { MaterialModule } from '../../material/material.module';
-import {MatDividerModule} from '@angular/material/divider';
+import { MatDividerModule } from '@angular/material/divider';
+import { User, UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [ NavbarComponent, MaterialModule, MatDividerModule ],
+  imports: [NavbarComponent, MaterialModule, MatDividerModule],
   templateUrl: './profile.component.html',
-  styleUrl: './profile.component.css'
+  styleUrl: './profile.component.css',
 })
 export class ProfileComponent {
+  user: User | undefined;
 
+  constructor(private userService: UserService) {}
+  async ngOnInit() {
+    await this.userService.fetchUsers();
+    this.user = this.userService.getUsers()[0];
+    console.log("this.user",this.user)
+  }
 }
