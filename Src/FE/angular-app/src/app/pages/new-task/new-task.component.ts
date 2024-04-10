7ac0a7f359215741721270043972a07c1124b74e
Strahinja Milenovic <59-2021@pmf.kg.ac.rs>
2024-04-10 11:23:19 +0200

    #113 update new task page logic

--- Src/FE/angular-app/src/app/pages/new-task/new-task.component.ts
+++ Src/FE/angular-app/src/app/pages/new-task/new-task.component.ts
@@ -38,6 +38,8 @@ import {
   styleUrl: './new-task.component.css',
 })
 export class NewTaskComponent {
+  projectId: number | undefined;
+
   errorMessage: string | null = null;
   title: string | null = null;
   description: string | null = null;
@@ -70,7 +72,17 @@ export class NewTaskComponent {
   constructor(
     private taskService: TaskService,
     private categoryService: CategoryService,
+    private route: ActivatedRoute,
+    private router: Router,
   ) {
+  }
+
+  async ngOnInit() {
+    this.route.params.subscribe((params) => {
+      this.projectId = parseInt(params['id']);
+    });
+    if(typeof this.projectId !== "number") return;
+    await this.taskService.fetchTasks({ projectId: this.projectId });
     this.categories = this.categoryService.getCategories().map((cat) => {
       return {
         value: cat.id.toString(),
@@ -105,118 +117,6 @@ export class NewTaskComponent {
       },
       1
     );
+    this.router.navigateByUrl(`/project/${this.projectId}/tasks`);
   }
 }
-
-// import { Component } from '@angular/core';
-// import { NavbarComponent } from '../../components/navbar/navbar.component';
-
-// @Component({
-//   selector: 'app-new-task',
-//   standalone: true,
-//   imports: [NavbarComponent],
-//   templateUrl: './new-task.component.html',
-//   styleUrl: './new-task.component.css',
-// })
-// export class NewTaskComponent {}
-
-// import { Component } from '@angular/core';
-// import { MaterialModule } from '../../material/material.module';
-// import { AuthService } from '../../services/auth.service';
-// import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
-// import { CommonModule } from '@angular/common';
-// import { ActivatedRoute, Router } from '@angular/router';
-// import { ClearableInputComponent } from '../../components/clearable-input/clearable-input.component';
-// import { EmailFieldComponent } from '../../components/email-field/email-field.component';
-// import { SelectComponent } from '../../components/select/select.component';
-// import { RolesService } from '../../services/roles.service';
-// import { Task, TaskService } from '../../services/task.service';
-// import { MatDialogRef } from '@angular/material/dialog';
-// import { CategoryService } from '../../services/category.service';
-// import { MAT_DATE_LOCALE, provideNativeDateAdapter } from '@angular/material/core';
-// @Component({
-//   selector: 'app-task-creation-modal',
-//   standalone: true,
-//   imports: [
-//     MaterialModule,
-//     FormsModule,
-//     ReactiveFormsModule,
-//     CommonModule,
-//     ClearableInputComponent,
-//     EmailFieldComponent,
-//     SelectComponent,
-//   ],
-//   templateUrl: './task-creation-modal.component.html',
-//   styleUrl: './task-creation-modal.component.css',
-//   providers: [ provideNativeDateAdapter(), { provide: MAT_DATE_LOCALE, useValue: 'en-GB' } ]
-// })
-// export class TaskCreationModalComponent {
-//   errorMessage: string | null = null;
-//   title: string | null = null;
-//   description: string | null = null;
-//   // date: string | null = null;
-//   // startDate: string | null = null;
-//   dueDate = new FormControl(new Date())
-//   startDate = new FormControl(new Date())
-//   priority: string | null = null;
-//   category: string | null = null;
-//   dependencies: string[] = [];
-
-//   priorities = [
-//     { value: 'Low', viewValue: 'Low' },
-//     { value: 'Medium', viewValue: 'Medium' },
-//     { value: 'High', viewValue: 'High' },
-//   ];
-//   categories: { value: string; viewValue: string }[] = [];
-//   tasks = this.taskService.getTasks().map(t => ({value: t.id, viewValue: t.title}));
-//   // get categories(){
-//   //   return this.categoryService.getCategories().map(cat => {
-//   //     return {
-//   //       value: cat.id.toString(),
-//   //       viewValue: cat.name
-//   //     }
-//   //   })
-//   // }
-
-//   constructor(
-//     private taskService: TaskService,
-//     private categoryService: CategoryService,
-//     public dialogRef: MatDialogRef<TaskCreationModalComponent>
-//   ) {
-//     this.categories = this.categoryService.getCategories().map((cat) => {
-//       return {
-//         value: cat.id.toString(),
-//         viewValue: cat.name,
-//       };
-//     });
-//   }
-
-//   async createTask() {
-//     if (
-//       !this.title ||
-//       !this.dueDate ||
-//       !this.priority ||
-//       !this.description ||
-//       !this.category ||
-//       !this.dueDate.value ||
-//       !this.startDate.value
-//     ) {
-//       this.errorMessage = 'Please provide all required fields';
-//       return;
-//     }
-//     await this.taskService.createTask(
-//       {
-//         title: this.title,
-//         description: this.description,
-//         date: this.dueDate.value,
-//         category: this.category,
-//         priority: this.priority as 'Low' | 'High' | 'Medium',
-//         status: 'Active',
-//         assignedTo: [],
-//         dependencies: this.dependencies,
-//       },
-//       1
-//     );
-//     this.dialogRef.close();
-//   }
-// }
