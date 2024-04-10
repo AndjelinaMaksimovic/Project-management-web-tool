6bf82e93f4c7bfda8da9522d4bbe3b22636bcabf
Strahinja Milenovic <59-2021@pmf.kg.ac.rs>
2024-04-10 09:52:45 +0200

    #127 create profile page header

--- Src/FE/angular-app/src/app/pages/profile/profile.component.ts
+++ Src/FE/angular-app/src/app/pages/profile/profile.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
+import { MaterialModule } from '../../material/material.module';
+import {MatDividerModule} from '@angular/material/divider';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [ NavbarComponent ],
+  imports: [ NavbarComponent, MaterialModule, MatDividerModule ],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.css'
 })
