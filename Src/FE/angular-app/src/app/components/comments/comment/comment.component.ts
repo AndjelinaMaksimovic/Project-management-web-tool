062b20f566042bf12816776f367aec4f4784ba7e
Strahinja Milenovic <59-2021@pmf.kg.ac.rs>
2024-04-11 14:23:17 +0200

    #131 add markdown support for comments

--- Src/FE/angular-app/src/app/components/comments/comment/comment.component.ts
+++ Src/FE/angular-app/src/app/components/comments/comment/comment.component.ts
@@ -1,11 +1,13 @@
 import { Component, Input } from '@angular/core';
 import { Comment } from '../../../services/comments.service';
 import { MaterialModule } from '../../../material/material.module';
+import { MarkdownModule, provideMarkdown } from 'ngx-markdown';
 
 @Component({
   selector: 'app-comment',
   standalone: true,
-  imports: [MaterialModule],
+  imports: [MaterialModule, MarkdownModule],
+  providers: [provideMarkdown()],
   templateUrl: './comment.component.html',
   styleUrl: './comment.component.css'
 })
