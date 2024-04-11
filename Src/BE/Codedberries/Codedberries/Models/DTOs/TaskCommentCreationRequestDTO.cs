040e0d0283e1934747b455d51f24b8e804b51f2f
Marko Nenadovic <67-2020@pmf.kg.ac.rs>
2024-04-11 13:28:17 +0200

    #139 dodat TaskCommentCreationRequestDTO

---
+++ Src/BE/Codedberries/Codedberries/Models/DTOs/TaskCommentCreationRequestDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace Codedberries.Models.DTOs
+{
+    public class TaskCommentCreationRequestDTO
+    {
+        public string Comment {  get; set; }
+        public int TaskId { get; set; }
+        public int UserId { get; set; }
+
+    }
+}
