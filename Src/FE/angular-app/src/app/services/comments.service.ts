a2ae1bbc49420c97e431a4cbea4447c0f7893876
Strahinja Milenovic <59-2021@pmf.kg.ac.rs>
2024-04-11 14:15:17 +0200

    #131 add dummy data to comments service

--- Src/FE/angular-app/src/app/services/comments.service.ts
+++ Src/FE/angular-app/src/app/services/comments.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
 
+export type Comment = {
+  author: string,
+  date: Date,
+  content: string,
+}
+
 @Injectable({
   providedIn: 'root'
 })
 export class CommentsService {
-
+  comments: Comment[] = [
+    {
+      author: "user1",
+      date: new Date(),
+      content: "This is the first comment"
+    },
+    {
+      author: "user1",
+      date: new Date(),
+      content: "test"
+    },
+  ];
+  getComments(){
+    return this.comments;
+  }
   constructor() { }
 }
