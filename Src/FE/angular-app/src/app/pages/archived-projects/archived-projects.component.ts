4ef7bc4b75366002b509bd272dfd39299104625d
uros.aksentijevic <24-2021@pmf.kg.ac.rs>
2024-04-13 00:28:24 +0200

    #135 Kreiranje stranice za arhivirane projekte

---
+++ Src/FE/angular-app/src/app/pages/archived-projects/archived-projects.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-archived-projects',
+  standalone: true,
+  imports: [],
+  templateUrl: './archived-projects.component.html',
+  styleUrl: './archived-projects.component.css'
+})
+export class ArchivedProjectsComponent {
+
+}
