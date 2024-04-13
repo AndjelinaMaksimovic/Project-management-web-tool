4aeb3210904e4f32fc133009762631183498b63f
Milica Rogic <milica.rog@gmail.com>
2024-04-13 08:41:57 +0200

    #145 Kreiran StatusOrderChangeDTO

---
+++ Src/BE/Codedberries/Codedberries/Models/DTOs/StatusOrderChangeDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace Codedberries.Models.DTOs
+{
+    public class StatusOrderChangeDTO
+    {
+        public int ProjectId { get; set; }
+        public List<int> NewOrder { get; set; }
+    }
+}
