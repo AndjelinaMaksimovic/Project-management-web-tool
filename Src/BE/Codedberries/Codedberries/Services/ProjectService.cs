bd4c70dde9e7652bb9aa2974a21cb7cc9b0cfd6e
Milica Rogic <milica.rog@gmail.com>
2024-04-15 10:06:24 +0200

    #152 Izmena kod CreateProject servisa

--- Src/BE/Codedberries/Codedberries/Services/ProjectService.cs
+++ Src/BE/Codedberries/Codedberries/Services/ProjectService.cs
@@ -120,21 +120,6 @@ namespace Codedberries.Services
                 _databaseContext.Projects.Add(project);
                 await _databaseContext.SaveChangesAsync();
 
-                // adding user to UserProjects table
-                foreach (int user_id in request.UserIds)
-                {
-                    var userToAddToProject = _databaseContext.Users.FirstOrDefault(u => u.Id == user_id);
-
-                    var userProject = new UserProject
-                    {
-                        UserId = userToAddToProject.Id,
-                        ProjectId = project.Id,
-                        RoleId = userToAddToProject.RoleId.Value
-                    };
-
-                    _databaseContext.UserProjects.Add(userProject);
-                }
-
                 await _statusService.CreateStatus(httpContext, new StatusCreationDTO { Name = "New", ProjectId = project.Id });
                 await _statusService.CreateStatus(httpContext, new StatusCreationDTO { Name = "In Progress", ProjectId = project.Id });
                 await _statusService.CreateStatus(httpContext, new StatusCreationDTO { Name = "Done", ProjectId = project.Id });
