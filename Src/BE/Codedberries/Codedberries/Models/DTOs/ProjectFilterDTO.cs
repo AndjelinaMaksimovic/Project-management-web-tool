706012fb9915091f86bbb07587ac74bf0a6b92e4
Milica Rogic <milica.rog@gmail.com>
2024-04-15 06:56:01 +0200

    #152 Azuriran ProjectFilterDTO

--- Src/BE/Codedberries/Codedberries/Models/DTOs/ProjectFilterDTO.cs
+++ Src/BE/Codedberries/Codedberries/Models/DTOs/ProjectFilterDTO.cs
@@ -3,8 +3,9 @@
     public class ProjectFilterDTO
     {
         public int? ProjectId { get; set; }
-        public int? AssignedTo { get; set; }
+        public List<int> AssignedTo { get; set; }
         public DateTime? DueDateAfter { get; set; }
         public DateTime? DueDateBefore { get; set; }
+        public bool? IsArchived { get; set; }
     }
 }
