4c2b6616f56227e7e6bd3575a86cd040a7dcfe76
Milica Rogic <milica.rog@gmail.com>
2024-04-17 19:35:49 +0200

    #155 Izmena kod allArchivedProjects u ProjectsController-u

--- Src/BE/Codedberries/Codedberries/Controllers/ProjectsController.cs
+++ Src/BE/Codedberries/Codedberries/Controllers/ProjectsController.cs
@@ -75,6 +75,10 @@ namespace Codedberries.Controllers
             {
                 return Unauthorized(new ErrorMsg(ex.Message));
             }
+            catch (ArgumentException ex)
+            {
+                return BadRequest(new ErrorMsg(ex.Message));
+            }
             catch (Exception ex)
             {
                 return StatusCode(500, new ErrorMsg($"An error occurred while fetching archived projects: {ex.Message}"));
