f9476ebbc77512e85af39165ecbf7fd25d4986aa
Strahinja Milenovic <59-2021@pmf.kg.ac.rs>
2024-04-18 10:28:42 +0200

    #131 add write comment field to comments component

--- Src/FE/angular-app/src/app/components/comments/comments/comments.component.ts
+++ Src/FE/angular-app/src/app/components/comments/comments/comments.component.ts
@@ -1,15 +1,17 @@
 import { Component, Input } from '@angular/core';
 import { Comment, CommentsService } from '../../../services/comments.service';
 import { CommentComponent } from '../comment/comment.component';
+import { MaterialModule } from '../../../material/material.module';
 
 @Component({
   selector: 'app-comments',
   standalone: true,
-  imports: [CommentComponent],
+  imports: [CommentComponent, MaterialModule],
   templateUrl: './comments.component.html',
   styleUrl: './comments.component.css'
 })
 export class CommentsComponent {
+  newComment: string = "";
   @Input() taskId: number | undefined;
   get comments(){
     return this.commentsService.getComments();
