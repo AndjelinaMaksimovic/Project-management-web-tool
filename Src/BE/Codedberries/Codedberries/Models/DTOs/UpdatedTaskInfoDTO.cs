a67368714f43154968eb905a7b2b19aa7e00d9c8
Milica Rogic <milica.rog@gmail.com>
2024-04-21 07:18:42 +0200

    #164 Izmena kod UpdatedTaskInfoDTO

--- Src/BE/Codedberries/Codedberries/Models/DTOs/UpdatedTaskInfoDTO.cs
+++ Src/BE/Codedberries/Codedberries/Models/DTOs/UpdatedTaskInfoDTO.cs
@@ -8,8 +8,9 @@
         public int CategoryId { get; set; }
         public int PriorityId { get; set; }
         public int StatusId { get; set; }
-        public DateTime DueDate { get; set; }
         public DateTime StartDate { get; set; }
+        public DateTime DueDate { get; set; }
+        public DateTime FinishedDate { get; set; };
         public int DifficultyLevel { get; set; }
         public int ProjectId { get; set; }
         public List<UserDTO> AssignedUsers { get; set; }
