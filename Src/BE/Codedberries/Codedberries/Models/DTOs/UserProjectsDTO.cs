8e2e43879321803b449402975dde46ad1be103a1
Milica Rogic <milica.rog@gmail.com>
2024-04-22 17:52:36 +0200

    #170 Kreiran UserProjectsDTO

---
+++ Src/BE/Codedberries/Codedberries/Models/DTOs/UserProjectsDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace Codedberries.Models.DTOs
+{
+    public class UserProjectsDTO
+    {
+        public int UserId { get; set; }
+        public int ProjectId { get; set; }
+        public int RoleId { get; set; }
+        public string RoleName { get; set; }
+    }
+}
