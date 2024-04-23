a38cd8d1cf73ef4de74607e012f5e0e428896ec3
Milica Rogic <milica.rog@gmail.com>
2024-04-23 17:02:13 +0200

    #172 Mala izmena kod TaskCreationRequestDTO

--- Src/BE/Codedberries/Codedberries/Models/DTOs/TaskCreationRequestDTO.cs
+++ Src/BE/Codedberries/Codedberries/Models/DTOs/TaskCreationRequestDTO.cs
@@ -6,8 +6,8 @@ namespace Codedberries.Models.DTOs
     {
         public string Name { get; set; }
         public string Description { get; set; }
-        public DateTime DueDate { get; set; }
         public DateTime StartDate { get; set; }
+        public DateTime DueDate { get; set; }      
         public int StatusId { get; set; }
         public int PriorityId { get; set; }
         public int DifficultyLevel { get; set; }
