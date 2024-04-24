1217f93f7c4fc2e82301d629b3c88679b48015be
Strahinja Milenovic <59-2021@pmf.kg.ac.rs>
2024-04-24 08:34:49 +0200

    #169 add markdown editor to post comment

--- Src/FE/angular-app/src/app/components/comments/comments/comments.component.ts
+++ Src/FE/angular-app/src/app/components/comments/comments/comments.component.ts
@@ -4,11 +4,12 @@ import { CommentComponent } from '../comment/comment.component';
 import { MaterialModule } from '../../../material/material.module';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { MarkdownEditorComponent } from '../../markdown-editor/markdown-editor.component';
 
 @Component({
   selector: 'app-comments',
   standalone: true,
-  imports: [CommentComponent, MaterialModule, FormsModule],
+  imports: [CommentComponent, MaterialModule, FormsModule, MarkdownEditorComponent],
   templateUrl: './comments.component.html',
   styleUrl: './comments.component.css'
 })
