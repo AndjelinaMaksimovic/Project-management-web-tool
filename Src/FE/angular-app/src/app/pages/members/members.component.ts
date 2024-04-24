795617b06c059106bbbfdef174cf99067b2311f7
uros.aksentijevic <24-2021@pmf.kg.ac.rs>
2024-04-24 19:27:24 +0200

    #144 Sredivanje stranice

--- Src/FE/angular-app/src/app/pages/members/members.component.ts
+++ Src/FE/angular-app/src/app/pages/members/members.component.ts
@@ -4,15 +4,21 @@ import { FiltersComponent, Filter } from '../../components/filters/filters.compo
 import { FormsModule } from '@angular/forms';
 import { KeyValuePipe, NgFor, NgIf } from '@angular/common';
 import { MemberItemComponent } from '../../components/member-item/member-item.component';
+import { RolesService } from '../../services/roles.service';
+import { UserService } from '../../services/user.service';
+import { UserStatsComponent } from '../../components/user-stats/user-stats.component';
+import { MatDialog } from '@angular/material/dialog';
 
 class Member {
-  name: string;
+  firstname: string;
+  lastname: string;
   id: number;
   image: string;
   projects: number;
 
-  constructor(name: string, id: number, image: string, projects: number) {
-    this.name = name;
+  constructor(firstname: string, lastname: string, id: number, image: string, projects: number) {
+    this.firstname = firstname;
+    this.lastname = lastname;
     this.id = id;
     this.image = image;
     this.projects = projects;
@@ -30,12 +36,16 @@ class Role {
     this.id = id;
     this.members = members;
   }
+
+  addMember(member: Member) {
+    this.members.push(member);
+  }
 }
 
 @Component({
   selector: 'app-members',
   standalone: true,
-  imports: [ TopnavComponent, FormsModule, FiltersComponent, NgIf, NgFor, MemberItemComponent, KeyValuePipe ],
+  imports: [ TopnavComponent, FormsModule, FiltersComponent, NgIf, NgFor, MemberItemComponent, KeyValuePipe, UserStatsComponent ],
   templateUrl: './members.component.html',
   styleUrl: './members.component.css'
 })
@@ -53,17 +63,24 @@ export class MembersComponent {
 
   isFilterOpen: boolean = false;
 
-  constructor() {}
+  constructor(private rolesService : RolesService, private userService: UserService, public dialog: MatDialog) {}
 
   get _roles() {
     return this.roles;
   }
 
   async ngOnInit(){
-    // this.roles.set(1, new Role("Project owner", 2, [ new Member("Test", 1, "https://img.freepik.com/free-photo/handsome-bearded-guy-posing-against-white-wall_273609-20597.jpg?size=626&ext=jpg&ga=GA1.1.1224184972.1713571200&semt=sph", 0), new Member("Test", 1, "", 2) ]));
-    // this.roles.set(2, new Role("Project manager", 2, [ new Member("Test", 1, "", 0)]));
-    // this.roles.set(3, new Role("Employee", 2, [ new Member("Test", 1, "", 0)]));
-    // this.roles.set(4, new Role("Viewer", 2, [ new Member("Test", 1, "", 0)]));
+    let onlyRoles = await this.rolesService.getAllRoles();
+    onlyRoles?.forEach((val, index) => {
+      this.roles.set(val.id, new Role(val.roleName, val.id, []));
+    });
+
+    await this.userService.fetchUsers();
+    let onlyUsers = await this.userService.getUsers();
+
+    onlyUsers?.forEach((val, index) => {
+      this.roles.get(val.id)?.addMember(new Member(val.firstName, val.lastName, val.id, val.profilePicture, 0));
+    });
   }
 
   async fetchMembersFromLocalStorage() {
@@ -79,5 +96,11 @@ export class MembersComponent {
 
   openFilters() {
     this.isFilterOpen = !this.isFilterOpen;
+  }
+
+  openMember(id: number) {
+    const dialogRef = this.dialog.open(UserStatsComponent, {
+      data: {id: id}
+    });
   }
 }
