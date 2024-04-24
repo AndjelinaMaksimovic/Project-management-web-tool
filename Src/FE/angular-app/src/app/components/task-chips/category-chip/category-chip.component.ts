718789bc3b360cea31b3d8d3310514f07356859f
Strahinja Milenovic <59-2021@pmf.kg.ac.rs>
2024-04-24 09:57:52 +0200

    #174 add category chip component

--- Src/FE/angular-app/src/app/components/task-chips/category-chip/category-chip.component.ts
+++ Src/FE/angular-app/src/app/components/task-chips/category-chip/category-chip.component.ts
@@ -1,13 +1,34 @@
 import { Component, Input } from '@angular/core';
-import { Task } from '../../../services/task.service';
+import { Task, TaskService } from '../../../services/task.service';
+import { MaterialModule } from '../../../material/material.module';
+import { CategoryService } from '../../../services/category.service';
 
 @Component({
   selector: 'app-category-chip',
   standalone: true,
-  imports: [],
+  imports: [MaterialModule],
   templateUrl: './category-chip.component.html',
   styleUrl: './category-chip.component.css'
 })
 export class CategoryChipComponent {
   @Input() task: Task | undefined;
+
+  constructor(private categoryService: CategoryService, private taskService: TaskService){}
+
+  get categorieNames(){
+    return this.categoryService.getCategories().map(c => c.name);
+  }
+
+  get categories(){
+    return this.categoryService.getCategories();
+  }
+
+  updateCategory(categoryId: string){
+    console.log(categoryId);
+    if(!this.task) return;
+    this.taskService.updateTask({
+      id: this.task.id,
+      categoryId: categoryId
+    })
+  }
 }
