65bc0574b8bc11fbca6b8abaeddbe594c9fc7d03
Milica Rogic <milica.rog@gmail.com>
2024-04-26 14:32:44 +0200

    #178 Kreiran nov servis UserProjectsService

---
+++ Src/BE/Codedberries/Codedberries/Services/UserProjectsService.cs
@@ -0,0 +1,14 @@
+﻿namespace Codedberries.Services
+{
+    public class UserProjectsService
+    {
+        private readonly AppDatabaseContext _databaseContext;
+        private readonly AuthorizationService _authorizationService;
+
+        public UserProjectsService(AppDatabaseContext databaseContext, AuthorizationService authorizationService)
+        {
+            _databaseContext = databaseContext;
+            _authorizationService = authorizationService;
+        }
+    }
+}
