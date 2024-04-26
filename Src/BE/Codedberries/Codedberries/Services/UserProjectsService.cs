07c1e4bffd4ab389a55d902f520752bdeaafc515
Milica Rogic <milica.rog@gmail.com>
2024-04-26 14:36:02 +0200

    #178 Dodate provere korisnika

--- Src/BE/Codedberries/Codedberries/Services/UserProjectsService.cs
+++ Src/BE/Codedberries/Codedberries/Services/UserProjectsService.cs
@@ -16,7 +16,24 @@ namespace Codedberries.Services
         // get all users that are on specific project
         public async Task<List<UserProjectsDTO>> GetUsersByProjectId(HttpContext httpContext, ProjectIdDTO request)
         {
+            var userId = _authorizationService.GetUserIdFromSession(httpContext);
 
+            if (userId == null)
+            {
+                throw new UnauthorizedAccessException("Invalid session!");
+            }
+
+            var user = _databaseContext.Users.FirstOrDefault(u => u.Id == userId);
+
+            if (user == null)
+            {
+                throw new UnauthorizedAccessException("User not found in database!");
+            }
+
+            if (user.RoleId == null)
+            {
+                throw new UnauthorizedAccessException("User does not have any role assigned!");
+            }
         }
     }
 }
