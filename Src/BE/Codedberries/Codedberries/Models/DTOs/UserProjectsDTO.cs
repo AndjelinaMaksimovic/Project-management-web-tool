42a345970fa5a58688eb32f7c18729b27debb25f
Milica Rogic <milica.rog@gmail.com>
2024-04-27 19:31:34 +0200

    #178 Azuriran UserProjectsDTO

--- Src/BE/Codedberries/Codedberries/Models/DTOs/UserProjectsDTO.cs
+++ Src/BE/Codedberries/Codedberries/Models/DTOs/UserProjectsDTO.cs
@@ -5,6 +5,7 @@
         public int UserId { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public string? ProfilePicture { get; set; }
         public int ProjectId { get; set; }
         public int RoleId { get; set; }
         public string RoleName { get; set; }
