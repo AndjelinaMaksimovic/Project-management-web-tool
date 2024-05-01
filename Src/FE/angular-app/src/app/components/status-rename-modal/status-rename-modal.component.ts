174b414e55bfa0d6d07937ee41da1bf97c2a9dae
Strahinja Milenovic <59-2021@pmf.kg.ac.rs>
2024-05-01 15:03:14 +0200

    #175 generate status rename modal component

---
+++ Src/FE/angular-app/src/app/components/status-rename-modal/status-rename-modal.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-status-rename-modal',
+  standalone: true,
+  imports: [],
+  templateUrl: './status-rename-modal.component.html',
+  styleUrl: './status-rename-modal.component.css'
+})
+export class StatusRenameModalComponent {
+
+}
