158f0f63d37147afd4d315b6a30883eadd8ddd6a
Strahinja Milenovic <59-2021@pmf.kg.ac.rs>
2024-05-01 15:05:53 +0200

    #175 implement rename status modal body

--- Src/FE/angular-app/src/app/components/status-rename-modal/status-rename-modal.component.ts
+++ Src/FE/angular-app/src/app/components/status-rename-modal/status-rename-modal.component.ts
@@ -1,12 +1,50 @@
 import { Component } from '@angular/core';
+import { MaterialModule } from '../../material/material.module';
+import { AuthService } from '../../services/auth.service';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ClearableInputComponent } from '../../components/clearable-input/clearable-input.component';
+import { EmailFieldComponent } from '../../components/email-field/email-field.component';
+import { SelectComponent } from '../../components/select/select.component';
+import { RolesService } from '../../services/roles.service';
+import { Task, TaskService } from '../../services/task.service';
+import { MatDialogRef } from '@angular/material/dialog';
+import { StatusService } from '../../services/status.service';
 
 @Component({
   selector: 'app-status-rename-modal',
   standalone: true,
-  imports: [],
+  imports: [
+    MaterialModule,
+    FormsModule,
+    CommonModule,
+    ClearableInputComponent,
+    EmailFieldComponent,
+    MaterialModule,
+    SelectComponent,
+  ],
   templateUrl: './status-rename-modal.component.html',
   styleUrl: './status-rename-modal.component.css'
 })
 export class StatusRenameModalComponent {
+  errorMessage: string | null = null;
+  name: string | null = null;
 
+  constructor(
+    private taskService: TaskService,
+    private statusService: StatusService,
+    public dialogRef: MatDialogRef<StatusRenameModalComponent>
+  ) {}
+
+  async createTask() {
+    if (
+      !this.name
+    ) {
+      this.errorMessage = 'Please provide all required fields';
+      return;
+    }
+    await this.statusService.createStatus(this.name);
+    this.dialogRef.close();
+  }
 }
