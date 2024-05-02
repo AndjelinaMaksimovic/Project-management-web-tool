ac8fc8588cf599c5a654a11f6aacd4e581e823b2
Milica Rogic <milica.rog@gmail.com>
2024-05-02 15:24:39 +0200

    #181 Dodat izuzetak

--- Src/BE/Codedberries/Codedberries/Controllers/UserProjectsController.cs
+++ Src/BE/Codedberries/Codedberries/Controllers/UserProjectsController.cs
@@ -52,6 +52,10 @@ namespace Codedberries.Controllers
                 
                 return Ok(userProjectsInformation);
             }
+            catch (UnauthorizedAccessException ex)
+            {
+                return Unauthorized(new ErrorMsg(ex.Message));
+            }
             catch (Exception ex)
             {
                 return StatusCode(500, new ErrorMsg($"An error occurred while fetching user projects: {ex.Message}"));
