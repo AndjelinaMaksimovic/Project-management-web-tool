0aec1ed215ac7da3f25b2033356ddeac64bc1cd5
Strahinja Milenovic <59-2021@pmf.kg.ac.rs>
2024-05-02 20:54:14 +0200

    #127 add working image upload

--- Src/FE/angular-app/src/app/pages/profile/profile.component.ts
+++ Src/FE/angular-app/src/app/pages/profile/profile.component.ts
@@ -4,6 +4,11 @@ import { MaterialModule } from '../../material/material.module';
 import { MatDividerModule } from '@angular/material/divider';
 import { User, UserService } from '../../services/user.service';
 import { ActivatedRoute } from '@angular/router';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { environment } from '../../../environments/environment';
+import { firstValueFrom } from 'rxjs';
+// import { environment } from '../../environments/environment';
+
 
 @Component({
   selector: 'app-profile',
@@ -16,11 +21,56 @@ export class ProfileComponent {
   userId: number = 0;
   user: User | undefined;
 
-  constructor(private userService: UserService, private route: ActivatedRoute) {}
+  
+  private httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+    withCredentials: true,
+    observe: 'response' as 'response',
+  };
+  constructor(
+    private userService: UserService,
+    private route: ActivatedRoute,
+    private http: HttpClient
+  ) {}
   async ngOnInit() {
     this.route.params.subscribe((params) => {
       this.userId = parseInt(params['userId']);
     });
     this.user = await this.userService.getUser(this.userId);
+  }
+
+  async sendDataToServer(data: {userId: string, imageBytes: string, imageName: string}){
+    console.log("sending data", data);
+    await firstValueFrom(this.http.post<any>(
+      environment.apiUrl + `/User/setProfilePicture`,
+      {
+        ...data
+      },
+      {
+        ...this.httpOptions,
+      }
+    ));
+  }
+  async uploadImage(base64String: string, imageName: string) {
+    const userId = "1"; // Replace with actual user ID
+    const data = {
+      userId: userId,
+      imageBytes: base64String,
+      imageName: imageName
+    };
+    await this.sendDataToServer(data);
+  }
+
+  onFileSelected(event: any) {
+    const file = event.target.files[0];
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onloadend = () => {
+      const base64String = reader.result?.toString().split(',')[1];
+      if(!base64String) return;
+      console.log("base64String", base64String);
+      // Now you have the base64 string of the image
+      this.uploadImage(base64String, "./test-image-02.jpg");
+    };
   }
 }
