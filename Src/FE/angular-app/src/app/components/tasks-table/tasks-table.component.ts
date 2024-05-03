9f5f0d20b46be3dd76cd214d23e3d7ae57cef2d3
Strahinja Milenovic <59-2021@pmf.kg.ac.rs>
2024-05-03 10:00:03 +0200

    #186 add paginator to task table view

--- Src/FE/angular-app/src/app/components/tasks-table/tasks-table.component.ts
+++ Src/FE/angular-app/src/app/components/tasks-table/tasks-table.component.ts
@@ -1,11 +1,12 @@
 import { Component, Input } from '@angular/core';
 import { TaskCardComponent } from '../task-card/task-card.component';
 import { Task } from '../../services/task.service';
+import { MaterialModule } from '../../material/material.module';
 
 @Component({
   selector: 'app-tasks-table',
   standalone: true,
-  imports: [TaskCardComponent],
+  imports: [TaskCardComponent, MaterialModule],
   templateUrl: './tasks-table.component.html',
   styleUrl: './tasks-table.component.css'
 })
