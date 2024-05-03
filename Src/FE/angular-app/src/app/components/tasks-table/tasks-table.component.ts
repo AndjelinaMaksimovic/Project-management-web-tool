55dba9746910ed3bd3bb173938d6300538ebbeb4
Strahinja Milenovic <59-2021@pmf.kg.ac.rs>
2024-05-03 10:08:19 +0200

    #186 add paginator to table view

--- Src/FE/angular-app/src/app/components/tasks-table/tasks-table.component.ts
+++ Src/FE/angular-app/src/app/components/tasks-table/tasks-table.component.ts
@@ -2,6 +2,7 @@ import { Component, Input } from '@angular/core';
 import { TaskCardComponent } from '../task-card/task-card.component';
 import { Task } from '../../services/task.service';
 import { MaterialModule } from '../../material/material.module';
+import { PageEvent } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-tasks-table',
@@ -12,4 +13,17 @@ import { MaterialModule } from '../../material/material.module';
 })
 export class TasksTableComponent {
   @Input() tasks!: Task[];
+  @Input() paginatedTasks!: Task[];
+
+  pageIndex: number = 0;
+  pageSize: number = 10;
+
+  pageChangeEvent(event: PageEvent){
+    this.pageIndex = event.pageIndex;
+    this.pageSize = event.pageSize;
+    const pageIndex = event.pageIndex;
+    const pageSize = event.pageSize;
+    this.paginatedTasks = this.tasks.slice(pageIndex * pageSize, pageIndex * pageSize + pageSize)
+    // this.getPagedData();
+  }
 }
