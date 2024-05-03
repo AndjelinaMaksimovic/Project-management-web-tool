6946bd1ab45226b339136c032b381a75bc714477
Strahinja Milenovic <59-2021@pmf.kg.ac.rs>
2024-05-03 09:22:13 +0200

    #127 generate editable name component

---
+++ Src/FE/angular-app/src/app/pages/profile/editable-name/editable-name.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-editable-name',
+  standalone: true,
+  imports: [],
+  templateUrl: './editable-name.component.html',
+  styleUrl: './editable-name.component.css'
+})
+export class EditableNameComponent {
+
+}
