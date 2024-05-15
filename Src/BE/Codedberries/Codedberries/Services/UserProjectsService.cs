636a258c74a65d82545baa4fefa7d3b5de9b1fe4
Milica Rogic <milica.rog@gmail.com>
2024-05-15 12:20:35 +0200

    #195 Izmena kod GetUserProjectsInformation servisa

--- Src/BE/Codedberries/Codedberries/Services/UserProjectsService.cs
+++ Src/BE/Codedberries/Codedberries/Services/UserProjectsService.cs
@@ -225,7 +225,7 @@ namespace Codedberries.Services
                     ProjectStartDate = project.StartDate,
                     ProjectDueDate = project.DueDate,
                     ProjectArchived = project.Archived,
-                    ProjectIsStarred = _databaseContext.Starred.Any(s => s.ProjectId == project.Id && s.UserId == userId),
+                    ProjectIsStarred = _databaseContext.Starred.Any(s => s.ProjectId == project.Id && s.UserId == request.UserId),
                     ProjectStatuses = projectStatuses,
                     ProjectCategories = projectCategories,
                     ProjectUsers = projectUsers
