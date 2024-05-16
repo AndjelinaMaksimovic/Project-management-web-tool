d0d1b20bd8c83059b5ad0d23c7a27e31f81b631a
Milica Rogic <milica.rog@gmail.com>
2024-05-16 06:40:03 +0200

    #203 Azuriran ProjectCreationRequestDTO

--- Src/BE/Codedberries/Codedberries/Models/DTOs/ProjectCreationRequestDTO.cs
+++ Src/BE/Codedberries/Codedberries/Models/DTOs/ProjectCreationRequestDTO.cs
@@ -8,6 +8,7 @@ namespace Codedberries.Models.DTOs
         public string Description { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime DueDate { get; set; }
-        public List<int> UserIds { get; set; }
+        
+        //public List<int> UserIds { get; set; }
     }
 }
