e02ca9ab3d8bbb989e6bdc3425e5d53743766493
Marko Nenadovic <67-2020@pmf.kg.ac.rs>
2024-05-17 02:39:54 +0200

    #201 dodat UpdateMilestone u MilestoneService

---
+++ Src/BE/Codedberries/Codedberries/Models/DTOs/MilestoneUpdateRequestDTO.cs
@@ -0,0 +1,14 @@
+﻿namespace Codedberries.Models.DTOs
+{
+    public class MilestoneUpdateRequestDTO
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public DateTime? Date { get; set; }
+
+        public bool IsEmpty() 
+        {
+            return string.IsNullOrEmpty(Name) && Date==null;
+        }
+    }
+}
