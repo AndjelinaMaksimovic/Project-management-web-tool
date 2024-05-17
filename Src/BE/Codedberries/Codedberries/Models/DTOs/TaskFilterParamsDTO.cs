f246a05dec825c6d23558a28b95bd281ab2298fd
Milica Rogic <milica.rog@gmail.com>
2024-05-17 07:21:35 +0200

    #199 Azuriran TaskFilterParamsDTO

--- Src/BE/Codedberries/Codedberries/Models/DTOs/TaskFilterParamsDTO.cs
+++ Src/BE/Codedberries/Codedberries/Models/DTOs/TaskFilterParamsDTO.cs
@@ -6,6 +6,7 @@
         public int? AssignedTo { get; set; } 
         public int? StatusId { get; set; } 
         public int? PriorityId { get; set; }
+        public string? TaskName {  get; set; }
         public int? DifficultyLevelGreaterThan { get; set; }
         public int? DifficultyLevelLesserThan { get; set; }
         public int? DifficultyLevelEquals { get; set; }
@@ -18,7 +19,7 @@
             return ProjectId == 0 && !AssignedTo.HasValue && !StatusId.HasValue
                 && !PriorityId.HasValue && !DifficultyLevelGreaterThan.HasValue &&
                 !DifficultyLevelLesserThan.HasValue && !DifficultyLevelEquals.HasValue &&
-                !DueDateAfter.HasValue && !DueDateBefore.HasValue && !CategoryId.HasValue;
+                !DueDateAfter.HasValue && !DueDateBefore.HasValue && !CategoryId.HasValue && string.IsNullOrEmpty(TaskName);
         }
     }
 }
