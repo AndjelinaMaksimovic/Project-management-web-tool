c8308e43a4490e88e7f237dd5a31915f8db6567a
Milica Rogic <milica.rog@gmail.com>
2024-05-17 21:38:56 +0200

    #205 Kreiran TaskDependencyRequestDTO

---
+++ Src/BE/Codedberries/Codedberries/Models/DTOs/TaskDependencyRequestDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace Codedberries.Models.DTOs
+{
+    public class TaskDependencyRequestDTO
+    {
+        public int TaskId1 { get; set; }
+        public int TaskId2 { get; set; }
+        public int TypeOfDependencyId { get; set; }
+    }
+}
