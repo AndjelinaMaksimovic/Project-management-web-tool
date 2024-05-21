4ea28871f339056481b0c1105c38f81d293f05cc
Marko Nenadovic <67-2020@pmf.kg.ac.rs>
2024-05-21 22:43:03 +0200

    #215 dodat model Activity

---
+++ Src/BE/Codedberries/Codedberries/Models/Activity.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Codedberries.Models
+{
+    [Table("Activities")]
+    public class Activity
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        
+        [Required]
+        public int ProjectId { get; set; }
+
+        [Required]
+        public int UserId { get; set; }
+
+        [Required]
+        public String ActivityDescription { get; set; }
+    }
+}
