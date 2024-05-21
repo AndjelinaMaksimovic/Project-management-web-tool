64194713949c5c7567f721d3a758cf3f33cc18c5
Milica Rogic <milica.rog@gmail.com>
2024-05-21 22:24:20 +0200

    #216 Azuriran GetTasksByFilters servis

---
+++ Src/BE/Codedberries/Codedberries/Models/DTOs/DependentTaskDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace Codedberries.Models.DTOs
+{
+    public class DependentTaskDTO
+    {
+        public int TaskId { get; set; }
+        public int TypeOfDependencyId { get; set; }
+    }
+}
