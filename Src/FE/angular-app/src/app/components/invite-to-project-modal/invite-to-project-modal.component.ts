db67df67087110747d4c031d4f73d7ff064565d2
Strahinja Milenovic <59-2021@pmf.kg.ac.rs>
2024-05-22 21:13:31 +0200

    #212 generate invite to project component

---
+++ Src/FE/angular-app/src/app/components/invite-to-project-modal/invite-to-project-modal.component.ts
@@ -0,0 +1,94 @@
+import { Component } from '@angular/core';
+import { MaterialModule } from '../../material/material.module';
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatListModule } from '@angular/material/list';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { InvitePopupComponent } from '../../components/invite-popup/invite-popup.component';
+import { TopnavComponent } from '../../components/topnav/topnav.component';
+import {
+  MAT_DATE_LOCALE,
+  provideNativeDateAdapter,
+} from '@angular/material/core';
+import { ProjectService } from '../../services/project.service';
+import { MatIconModule } from '@angular/material/icon';
+import { NgIf } from '@angular/common';
+import { MarkdownEditorComponent } from '../markdown-editor/markdown-editor.component';
+import moment from 'moment';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-invite-to-project-modal',
+  standalone: true,
+  templateUrl: './invite-to-project-modal.component.html',
+  styleUrl: './invite-to-project-modal.component.css',
+  imports: [
+    MarkdownEditorComponent,
+    MaterialModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatListModule,
+    TopnavComponent,
+    MatDatepickerModule,
+    MatIconModule,
+    NgIf,
+  ],
+})
+export class InviteToProjectModalComponent {
+  constructor(
+    private dialogue: MatDialog,
+    private projectService: ProjectService,
+    private dialogRef: MatDialogRef<InviteToProjectModalComponent>,
+    private router: Router,
+  ) {}
+
+  projectName = '';
+  description = '';
+  errorMessage = '';
+
+  currentDate = moment();
+  dueDate = new FormControl(this.currentDate);
+  startDate = new FormControl(this.currentDate);
+
+  async createNewProject() {
+    if (
+      !this.projectName ||
+      !this.dueDate ||
+      !this.description ||
+      !this.dueDate.value ||
+      !this.startDate.value
+    ) {
+      this.errorMessage = 'Please provide all required fields';
+      return;
+    }
+    if (
+      this.startDate.value.toDate().getTime() >
+      this.dueDate.value.toDate().getTime()
+    ) {
+      this.errorMessage = 'Please enter valid start/due dates';
+      return;
+    }
+    await this.projectService.createNew({
+      name: this.projectName,
+      description: this.description,
+      startDate: this.startDate.value.toDate(),
+      dueDate: this.dueDate.value.toDate(),
+    });
+    this.router.navigateByUrl(`/project/${1}`);
+    return;
+  }
+}
+
+
+// import { Component } from '@angular/core';
+
+// @Component({
+//   selector: 'app-invite-to-project-modal',
+//   standalone: true,
+//   imports: [],
+//   templateUrl: './invite-to-project-modal.component.html',
+//   styleUrl: './invite-to-project-modal.component.css'
+// })
+// export class InviteToProjectModalComponent {
+
+// }
