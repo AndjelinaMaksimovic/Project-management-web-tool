faf7c14aafe2bac5c83a1a89201bf47c3713b609
luka.zecevic <44-2020@pmf.kg.ac.rs>
2024-05-23 00:57:41 +0200

    #220 dodat prikaz notifikacija

--- Src/FE/angular-app/src/app/services/project.service.ts
+++ Src/FE/angular-app/src/app/services/project.service.ts
@@ -289,4 +289,24 @@ export class ProjectService {
     }
     return [];
   }
+  
+  async allUsersProjectActivities(): Promise<any[]> {
+    try {
+      const res = await firstValueFrom(
+        this.http.post<any>(
+          environment.apiUrl +
+            `/Projects/allUsersProjectActivities`,
+            {},
+            {
+              ...environment.httpOptions,
+            }
+        )
+      );
+      return res.body;
+    } catch (e) {
+      console.log(e);
+    }
+    return [];
+  }
+
 }
