764a68ebf71eb80cc8b0cd6209b9f34fe75de186
luka.zecevic <44-2020@pmf.kg.ac.rs>
2024-05-23 01:52:43 +0200

    #220 notification style

--- Src/FE/angular-app/src/app/components/notification-icon/notification-icon.component.ts
+++ Src/FE/angular-app/src/app/components/notification-icon/notification-icon.component.ts
@@ -1,16 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { MaterialModule } from '../../material/material.module';
 import { CommonModule } from '@angular/common';
 import { MatMenuModule } from '@angular/material/menu';
-import { ActivityItemComponent } from '../activity-item/activity-item.component';
+import { ProjectService } from '../../services/project.service';
+import { NotificationItemComponent } from '../notification-item/notification-item.component';
 
 @Component({
   selector: 'app-notification-icon',
   standalone: true,
-  imports: [ MaterialModule, CommonModule, MatMenuModule, ActivityItemComponent ],
+  imports: [ MaterialModule, CommonModule, MatMenuModule, NotificationItemComponent ],
   templateUrl: './notification-icon.component.html',
   styleUrl: './notification-icon.component.css'
 })
-export class NotificationIconComponent {
-  activities = []
+export class NotificationIconComponent implements OnInit, OnDestroy {
+  activities: any[] = []
+  menuOpened = false
+  intervalId!: any
+  
+
+  constructor(private projectService: ProjectService){}
+
+  async ngOnInit(){
+    this.activities = await this.projectService.allUsersProjectActivities()
+    this.intervalId = setInterval(async () => {
+      if(!this.menuOpened){
+        console.log("fetch")
+        this.activities = await this.projectService.allUsersProjectActivities()
+      }
+    }, 10_000)
+  }
+
+  ngOnDestroy(): void {
+    clearInterval(this.intervalId)
+  }
 }
