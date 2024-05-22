2ebbff4cc75aa058318ea2ca70484939b0bc2ce1
Milica Rogic <milica.rog@gmail.com>
2024-05-22 02:02:13 +0200

    #222 Azuriran TaskUpdateRequestDTO

--- Src/BE/Codedberries/Codedberries/Models/DTOs/TaskUpdateRequestDTO.cs
+++ Src/BE/Codedberries/Codedberries/Models/DTOs/TaskUpdateRequestDTO.cs
@@ -8,8 +8,8 @@
         public int? CategoryId { get; set; }
         public int? PriorityId { get; set; }
         public int? StatusId { get; set; }
-        public DateTime? DueDate { get; set; }
         public DateTime? StartDate { get; set; }
+        public DateTime? DueDate { get; set; }
         public int? UserId { get; set; }
         public int? DifficultyLevel { get; set; }
         public int? ProjectId { get; set; }
