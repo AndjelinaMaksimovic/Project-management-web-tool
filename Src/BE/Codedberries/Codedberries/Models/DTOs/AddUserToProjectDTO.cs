5c3bf7aaa751425422d4bb38738ea620fb99e66d
Milica Rogic <milica.rog@gmail.com>
2024-05-23 20:35:25 +0200

    #226 Kreiran AddUserToProjectDTO

---
+++ Src/BE/Codedberries/Codedberries/Models/DTOs/AddUserToProjectDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace Codedberries.Models.DTOs
+{
+    public class AddUserToProjectDTO
+    {
+        public int UserId { get; set; }
+        public int ProjectId { get; set; }
+        public int RoleId { get; set; }
+    }
+}
