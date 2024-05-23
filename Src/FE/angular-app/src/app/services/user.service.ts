40b6d50e25fd55c34c4ef883318f94f41c0df3d8
uros.aksentijevic <24-2021@pmf.kg.ac.rs>
2024-05-23 13:48:12 +0200

    #204 Resavanje konflikta

--- Src/FE/angular-app/src/app/services/user.service.ts
+++ Src/FE/angular-app/src/app/services/user.service.ts
@@ -81,6 +81,22 @@ export class UserService {
     return false;
   }
 
+  public async fetchUsersByProject(projectId: number) {
+    try {
+      const res = await firstValueFrom(
+        this.http.get<any>(
+          environment.apiUrl + `/User/getUsers?ProjectId=${projectId}`,
+          this.httpOptions
+        )
+      );
+      this.users = res.body.map((user: any) => {
+        return mapUser(user);
+      });
+    } catch (e) {
+      console.log(e);
+    }
+    return false;
+  }
   public async getUser(userId: number) {
     try {
       const res = await firstValueFrom(
