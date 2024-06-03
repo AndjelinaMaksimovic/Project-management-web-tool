ee8cee4b4bab3f9ac1890076158bec27b15f304f
luka.zecevic <44-2020@pmf.kg.ac.rs>
2024-06-03 16:21:50 +0200

    #212 task assignedTo fixed

--- Src/FE/angular-app/src/app/components/task-chips/add-user-chip/add-user-chip.component.ts
+++ Src/FE/angular-app/src/app/components/task-chips/add-user-chip/add-user-chip.component.ts
@@ -18,13 +18,15 @@ export class AddUserChipComponent {
   constructor(private userService: UserService, private taskService: TaskService){}
 
   users: any[] = []
+  avaliableUsers: any = []
   userVal: any
 
   async getUsers(){
     if(!this.task || !this.task.projectId)
       return
     await this.userService.fetchUsersByProject(this.task.projectId)
-    this.users = this.userService.getUsers().filter(allUsers => !this.task?.assignedTo.find((taskUsers: any) => allUsers.id == taskUsers.id))
+    this.users = this.task?.assignedTo
+    this.avaliableUsers = this.userService.getUsers().filter(allUsers => !this.task?.assignedTo.find((taskUsers: any) => allUsers.id == taskUsers.id))
   }
   // get user(){
   //   return this.userVal
@@ -36,12 +38,14 @@ export class AddUserChipComponent {
   async addUser(user: any){
     if(!this.task)
       return
+    console.log(this.users)
 
     const tmp = [...this.users]
     tmp.push(user)
+    console.log(tmp)
     
-    // if(await this.taskService.updateTask({id: this.task.id, userIds: tmp.map(usr => usr.id)}))
-    if(await this.taskService.updateTask({id: this.task.id, userId: tmp[0].id})){
+    if(await this.taskService.updateTask({id: this.task.id, userIds: tmp.map(usr => usr.id)})){
+    // if(await this.taskService.updateTask({id: this.task.id, userId: tmp[0].id})){
       this.users = tmp
       this.userOutput.emit(this.users)
     }
